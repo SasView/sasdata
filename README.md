18b9911bbef055be0482915941c8132647499b2e
krzywon <krzywon@yahoo.com>
2021-12-28 17:41:42 -0500

    Create a setup script, add a version number, and update the readme to include installation instructions.

--- README.md
+++ README.md
@@ -2,17 +2,31 @@
 
 A package for loading and handling reduced small angle scattering data.
 
-The data loaders provided are usable as a standalone package or in conjunction with the sasmodels fitting package, or
-the sasview analysis package.
+The data loaders provided are usable as a standalone package, in conjunction with the sasmodels fitting package, or
+in conjunction with the sasview analysis package.
+
+### *The following instructions are a work in progress and will be updated as this package progresses.*
 
 ## Install
-The easiest way to use `sasdata` is from [SasView](http://www.sasview.org).
+The easiest way to use sasdata is by using [SasView](http://www.sasview.org).
 
-*Standalone `sasdata` installation method and instructions coming soon.*
+You can also install sasdata as a standalone package in python. Use
+[miniconda](https://docs.conda.io/en/latest/miniconda.html)
+or [anaconda](https://www.anaconda.com/)
+to create a python environment with the sasdata dependencies::
 
-## Supported Modules
+    $ conda create -n sasdata -c conda-forge numpy lxml h5py
 
-*Supported module documentation coming soon.*
+The option ``-n sasdata`` names the environment sasdata, and the option
+``-c conda-forge`` selects the conda-forge package channel because pyopencl
+is not part of the base anaconda distribution.
+
+Activate the environment and install sasdata::
+
+    $ conda activate sasdata
+    (sasdata) $ pip install sasdata
+
+View the latest release on the [sasdata pypi page](https://pypi.org/project/sasdata/).
 
 ## Usage
 
---
+++ setup.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+"""
+    Setup for SasData
+"""
+import os
+import subprocess
+import shutil
+import sys
+from distutils.core import Command
+
+from setuptools import setup
+
+# Manage version number ######################################
+with open(os.path.join("sasdata", "__init__.py")) as fid:
+    for line in fid:
+        if line.startswith('__version__'):
+            print(line)
+            VERSION = line.split('"')[1]
+            break
+    else:
+        raise ValueError("Could not find version in src/sas/sasview/__init__.py")
+##############################################################
+
+package_dir = {}
+package_data = {}
+packages = []
+ext_modules = []
+
+# Remove all files that should be updated by this setup
+# We do this here because application updates these files from .sasdata
+# except when there is no such file
+
+CURRENT_SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+SAS_DATA_BUILD = os.path.join(CURRENT_SCRIPT_DIR, "build")
+
+# Optionally clean before build.
+dont_clean = 'update' in sys.argv
+if dont_clean:
+    sys.argv.remove('update')
+elif os.path.exists(SAS_DATA_BUILD):
+    print("Removing existing build directory", SAS_DATA_BUILD, "for a clean build")
+    shutil.rmtree(SAS_DATA_BUILD)
+
+
+class BuildSphinxCommand(Command):
+    description = "Build Sphinx documentation."
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = None
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        """Build the docs from source"""
+        sys.path.append("docs/sphinx-docs")
+        import build_sphinx
+        build_sphinx.rebuild()
+
+
+# _standard_ commands which should trigger the Qt build
+build_commands = [
+    'install', 'build', 'build_py', 'bdist', 'bdist_egg', 'bdist_rpm',
+    'bdist_wheel', 'develop', 'test'
+]
+
+# sasdata module
+package_dir["sasdata"] = os.path.join("sasdata")
+packages.append("sasdata")
+
+# sas.sascalc.dataloader
+package_dir["sasdata.dataloader"] = os.path.join("sasdata", "dataloader")
+package_data["sasdata.dataloader.readers"] = ['schema/*.xsd']
+packages.extend(["sasdata.dataloader", "sasdata.dataloader.readers", "sasdata.dataloader.readers.schema"])
+
+
+# sas.sascalc.file_converter
+package_dir["sasdata.file_converter"] = os.path.join("sasdata", "file_converter")
+packages.append("sasdata.file_converter")
+
+required = ['lxml', 'h5py']
+
+if os.name == 'nt':
+    required.extend(['html5lib', 'reportlab'])
+else:
+    # 'pil' is now called 'pillow'
+    required.extend(['pillow'])
+
+# Set up SasView
+setup(
+    name="sasdata",
+    version=VERSION,
+    description="SasData Loader application",
+    author="SasView Team",
+    author_email="developers@sasview.org",
+    url="http://sasview.org",
+    license="PSF",
+    keywords="small-angle x-ray and neutron scattering data loading",
+    download_url="https://github.com/SasView/sasdata.git",
+    package_dir=package_dir,
+    packages=packages,
+    package_data=package_data,
+    ext_modules=ext_modules,
+    install_requires=required,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            "sasview=sasdata.loader.Loader:load",
+        ]
+    }
+)
