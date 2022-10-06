584ba4008a1763449b7b23b92406ef812d8c6968
krzywon <krzywon@yahoo.com>
2022-10-06 16:08:46 -0400

    Fix deprecation decorator so function is properly returned and modify behavior to log warnings instead of throwing errors.

--- sasdata/data_util/deprecation.py
+++ sasdata/data_util/deprecation.py
@@ -1,18 +1,18 @@
-import warnings
+import logging
+import functools
 
 # TODO: Anything deprecated should have that in the docstring!!!!! -- @pkienzle
 
+logger = logging.getLogger(__name__)
+
 
 def deprecated(replaced_with=None):
-    """This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emitted
+    """This is a decorator which can be used to mark functions as deprecated. It will result in a warning being emitted
     when the function is used."""
+    @functools.wraps
     def decorator(func):
         def new_func(*args, **kwargs):
-            warnings.simplefilter('once', DeprecationWarning)  # turn off filter
-            warnings.warn(f"Call to deprecated function {func.__name__}. Call {replaced_with} in the future.",
-                          category=DeprecationWarning,
-                          stacklevel=2)
-            warnings.simplefilter('default', DeprecationWarning)  # reset filter
+            logger.warning(f"Call to deprecated function {func.__name__}. Call {replaced_with} in the future.")
             return func(*args, **kwargs)
+        return new_func
     return decorator
