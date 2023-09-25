3ac0415443cd250fc08f8e78ca4900ae7571202a
lucas-wilkins <lucas.wilkins.email@gmail.com>
2023-09-25 01:28:00 +0100

    Mesh merging works

--- test/slicers/meshes_for_testing.py
+++ test/slicers/meshes_for_testing.py
@@ -4,9 +4,9 @@ Meshes used in testing along with some expected values
 
 import numpy as np
 
-from sasdata.slicing.meshes.voronoi_mesh import voronoi_mesh
-from sasdata.slicing.meshes.mesh import Mesh
-from sasdata.slicing.meshes.meshmerge import meshmerge
+from sasdata.data_util.slicing.meshes.voronoi_mesh import voronoi_mesh
+from sasdata.data_util.slicing.meshes.mesh import Mesh
+from sasdata.data_util.slicing.meshes.meshmerge import meshmerge
 
 coords = np.arange(-4, 5)
 grid_mesh = voronoi_mesh(*np.meshgrid(coords, coords))
@@ -32,61 +32,25 @@ shape_mesh = Mesh(mesh_points, cells)
 
 # Subset of the mappings that meshmerge should include
 # This can be read off the plots generated below
-
-
 expected_shape_mappings = [
-    (100, -1),
-    (152, -1),
-    (141, -1),
-    (172, -1),
-    (170, -1),
-    (0, -1),
+    (98, -1),
+    (99, -1),
+    (12, 0),
     (1, -1),
-    (8, 0),
-    (9, 0),
-    (37, 0),
-    (83, 0),
-    (190, 1),
-    (186, 1),
-    (189, 1),
-    (193, 1)
-]
+    (148, 1),
+    (149, 1),
+    (110, 1),
+    (144, -1),
+    (123, -1)]
+
 
 expected_grid_mappings = [
-    (89, 0),
-    (90, 1),
-    (148, 16),
-    (175, 35),
-    (60, 47),
-    (44, 47),
-    (80, 60)
+    (89, 1),
+    (146, 29),
+    (66, 34),
+    (112, 45)
 ]
 
-#
-# Mesh location tests
-#
-
-location_test_mesh_points = np.array([
-    [0, 0],  # 0
-    [0, 1],  # 1
-    [0, 2],  # 2
-    [1, 0],  # 3
-    [1, 1],  # 4
-    [1, 2],  # 5
-    [2, 0],  # 6
-    [2, 1],  # 7
-    [2, 2]], dtype=float)
-
-location_test_mesh_cells = [
-    [0, 1, 4, 3],
-    [1, 2, 5, 4],
-    [3, 4, 7, 6],
-    [4, 5, 8, 7]]
-
-location_test_mesh = Mesh(location_test_mesh_points, location_test_mesh_cells)
-
-test_coords = 0.25 + 0.5*np.arange(4)
-location_test_points_x, location_test_points_y = np.meshgrid(test_coords, test_coords)
 
 if __name__ == "__main__":
 
@@ -98,18 +62,14 @@ if __name__ == "__main__":
     combined_mesh.show(actually_show=False, show_labels=True, color='k')
     grid_mesh.show(actually_show=False, show_labels=True, color='r')
 
-    plt.xlim([-5, 5])
-    plt.ylim([-5, 5])
+    plt.xlim([-4, 4])
+    plt.ylim([-4, 4])
 
     plt.figure()
     combined_mesh.show(actually_show=False, show_labels=True, color='k')
     shape_mesh.show(actually_show=False, show_labels=True, color='r')
 
-    plt.xlim([-5, 5])
-    plt.ylim([-5, 5])
-
-    plt.figure()
-    location_test_mesh.show(actually_show=False, show_labels=True)
-    plt.scatter(location_test_points_x, location_test_points_y)
+    plt.xlim([-4, 4])
+    plt.ylim([-4, 4])
 
     plt.show()
