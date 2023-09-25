5e809eac30598f2a105e05762194e8c8d116fd49
lucas-wilkins <lucas.wilkins.email@gmail.com>
2023-09-25 01:28:00 +0100

    Mesh merging works

---
+++ sasdata/data_util/slicing/meshes/util.py
@@ -0,0 +1,10 @@
+from typing import Sequence, TypeVar
+
+T = TypeVar("T")
+
+def closed_loop_edges(values: Sequence[T]) -> tuple[T, T]:
+    """ Generator for a closed loop of edge pairs """
+    for pair in zip(values, values[1:]):
+        yield pair
+
+    yield values[-1], values[0]
