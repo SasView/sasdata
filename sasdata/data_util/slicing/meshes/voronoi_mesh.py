5eb11691fb6cbbcf171239857b919014a9ae53c0
lucas-wilkins <lucas.wilkins.email@gmail.com>
2023-09-28 10:06:44 +0100

    Work towards demo

--- sasdata/data_util/slicing/meshes/voronoi_mesh.py
+++ sasdata/data_util/slicing/meshes/voronoi_mesh.py
@@ -7,9 +7,20 @@ from sasdata.data_util.slicing.meshes.mesh import Mesh
 def voronoi_mesh(x, y) -> Mesh:
 
     input_data = np.array((x.reshape(-1), y.reshape(-1))).T
+
+    # Need to make sure mesh covers a finite region, probably not important for
+    # much data stuff, but is important for plotting
+    #   To do this first need to find an appropriate region
+    #   Then we need to adjust the mesh to deal with these points
+
     voronoi = Voronoi(input_data)
 
+
+
+
     finite_cells = [region for region in voronoi.regions if -1 not in region and len(region) > 0]
+
+
 
     return Mesh(points=voronoi.vertices, cells=finite_cells)
 
