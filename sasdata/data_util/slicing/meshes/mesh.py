adc3fc5c3a7193e10ff6c5dccfe446616d2b9ef6
lucas-wilkins <lucas.wilkins.email@gmail.com>
2023-09-29 14:47:01 +0100

    Much faster assignment/merge method

--- sasdata/data_util/slicing/meshes/mesh.py
+++ sasdata/data_util/slicing/meshes/mesh.py
@@ -51,19 +51,24 @@ class Mesh:
 
         edge_lookup = {edge: i for i, edge in enumerate(self.edges)}
         self.cells_to_edges = []
+        self.cells_to_edges_signs = []
 
         for cell in cells:
 
             this_cell_data = []
+            this_sign_data = []
 
             for a, b in closed_loop_edges(cell):
                 # make sure the representation is unique
                 if a > b:
                     this_cell_data.append(edge_lookup[(a, b)])
+                    this_sign_data.append(1)
                 else:
                     this_cell_data.append(edge_lookup[(b, a)])
+                    this_sign_data.append(-1)
 
             self.cells_to_edges.append(this_cell_data)
+            self.cells_to_edges_signs.append(this_sign_data)
 
         # Counts for elements
         self.n_points = self.points.shape[0]
@@ -72,11 +77,6 @@ class Mesh:
 
         # Areas
         self._areas = None
-
-    def find_locations(self, points):
-        """ Find indices of cells containing the input points """
-
-
 
 
     @property
@@ -126,6 +126,71 @@ class Mesh:
         if actually_show:
             plt.show()
 
+    def locate_points(self, x: np.ndarray, y: np.ndarray):
+        """ Find the cells that contain the specified points"""
+
+        x = x.reshape(-1)
+        y = y.reshape(-1)
+
+        xy = np.concatenate(([x], [y]), axis=1)
+
+        # The most simple implementation is not particularly fast, especially in python
+        #
+        # Less obvious, but hopefully faster strategy
+        #
+        # Ultimately, checking the inclusion of a point within a polygon
+        # requires checking the crossings of a half line with the polygon's
+        # edges.
+        #
+        # A fairly efficient thing to do is to check every edge for crossing
+        # the axis parallel lines x=point_x.
+        # Then these edges that cross can map back to the polygons they're in
+        # and a final check for inclusion can be done with the edge sign property
+        # and some explicit checking of the
+        #
+        # Basic idea is:
+        #  1) build a matrix for each point-edge pair
+        #     True if the edge crosses the half-line above a point
+        #  2) for each cell get the winding number by evaluating the
+        #     sum of the component edges, weighted 1/-1 according to direction
+
+
+        edges = np.array(self.edges)
+
+        edge_xy_1 = self.points[edges[:, 0], :]
+        edge_xy_2 = self.points[edges[:, 1], :]
+
+        edge_x_1 = edge_xy_1[:, 0]
+        edge_x_2 = edge_xy_2[:, 0]
+
+
+
+        # Make an n_edges-by-n_inputs boolean matrix that indicates which of the
+        # edges cross x=points_x line
+        crossers = np.logical_xor(
+                        edge_x_1.reshape(-1, 1) < x.reshape(1, -1),
+                        edge_x_2.reshape(-1, 1) < x.reshape(1, -1))
+
+        # Calculate the gradients, some might be infs, but none that matter will be
+        # TODO: Disable warnings
+        gradients = (edge_xy_2[:, 1] - edge_xy_1[:, 1]) / (edge_xy_2[:, 0] - edge_xy_1[:, 0])
+
+        # Distance to crossing points edge 0
+        delta_x = x.reshape(1, -1) - edge_x_1.reshape(-1, 1)
+
+        # Signed distance from point to y (doesn't really matter which sign)
+        delta_y = gradients.reshape(-1, 1) * delta_x + edge_xy_1[:, 1:] - y.reshape(1, -1)
+
+        score_matrix = np.logical_and(delta_y > 0, crossers)
+
+        output = -np.ones(len(x), dtype=int)
+        for cell_index, (cell_edges, sign) in enumerate(zip(self.cells_to_edges, self.cells_to_edges_signs)):
+            cell_score = np.sum(score_matrix[cell_edges, :] * np.array(sign).reshape(-1, 1), axis=0)
+            points_in_cell = np.abs(cell_score) == 1
+            output[points_in_cell] = cell_index
+
+        return output
+
     def show_data(self,
                   data: np.ndarray,
                   cmap='winter',
@@ -157,3 +222,19 @@ class Mesh:
 
         if actually_show:
             self.show()
+
+
+if __name__ == "__main__":
+    from test.slicers.meshes_for_testing import location_test_mesh, location_test_points_x, location_test_points_y
+
+    cell_indices = location_test_mesh.locate_points(location_test_points_x, location_test_points_y)
+
+    print(cell_indices)
+
+    for i in range(location_test_mesh.n_cells):
+        inds = cell_indices == i
+        plt.scatter(
+            location_test_points_x.reshape(-1)[inds],
+            location_test_points_y.reshape(-1)[inds])
+
+    location_test_mesh.show()
