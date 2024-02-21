ff1c65ce54c2921546bd94b95be0ecb296f39d07
jkrzywon <krzywon@yahoo.com>
2024-02-21 08:35:46 -0500

    Rename import and export to data_import and data_export

--- sasdata/data_io/data_import.py
+++ sasdata/data_io/data_import.py
@@ -9,13 +9,6 @@
     a directory (for instance, a user plug-in directory) and
     look for new readers/writers.
 """
-#####################################################################
-# This software was developed by the University of Tennessee as part of the
-# Distributed Data Analysis of Neutron Scattering Experiments (DANSE)
-# project funded by the US National Science Foundation.
-# See the license text in license.txt
-# copyright 2008, University of Tennessee
-######################################################################
 
 import logging
 from types import ModuleType
