b71defb96e8b04982fc5df8d773f85196857d6a9
krzywon <krzywon@yahoo.com>
2024-03-21 10:12:56 -0400

    Cleanup associations to match actual behavior

--- sasdata/data_io/associations.py
+++ sasdata/data_io/associations.py
@@ -2,7 +2,6 @@ from typing import TYPE_CHECKING, ClassVar
 from importlib import import_module
 import sasdata.data_io.importers as importers
 
-
 if TYPE_CHECKING:
     from sasdata.data_io.data_import import Registry
     from sasdata.data_io.importer import Importer
@@ -14,16 +13,16 @@ def import_associations(obj: Registry):
     obj.modules = find_importers()
 
 
-def find_modules(package_name: str, cls_name: str) -> dict(str, ClassVar):
-    return {"Yo!": import_module(cls_name, package_name)}
+def find_module(package_name: str, cls_name: str) -> dict[str, ClassVar]:
+    return {cls_name: import_module(cls_name, package_name)}
 
 
-def find_importers() -> dict(str, Importer):
+def find_importers() -> dict[str, Importer]:
     for module in importers.__all__:
         try:
-            import_module('Importer', f'sasdata.data_io.importers.{module}')
-            MODULES[Importer.__name__] = module
+            loaded_mod = find_module('Importer', f'sasdata.data_io.importers.{module}')
+            MODULES.update(loaded_mod)
         except ImportError:
             # Modules without an Importer object should throw this error
             pass
-    return {}
+    return MODULES
