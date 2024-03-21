58815712e1ca6fe3c9a1e3bf395526e1d68927b5
krzywon <krzywon@yahoo.com>
2024-03-21 16:36:17 -0400

    Create dataclasses for Scalar and Vector conditions and a unit-scalable condition

--- sasdata/condition/condition_meta.py
+++ sasdata/condition/condition_meta.py
@@ -3,58 +3,49 @@ from typing import Union, Optional
 
 import numpy as np
 
-from sasdata.data_util.nxsunit import Converter, standardize_units
+from sasdata.data_util.nxsunit import Converter
 
 SCALAR_TYPE = Union[float, int, str]
 VALUE_TYPE = Union[str, float, list[str], np.ndarray]
 
 
-class ConditionBase:
-    pass
+@dataclass
+class ConditionScalar:
+    """A scalar value for a condition"""
+    # The name of the condition
+    name: str
+    # The value for the condition
+    value: SCALAR_TYPE
+    # The uncertainty for the condition
+    uncertainty: Optional[VALUE_TYPE]
+    # The description of the scalar quantity
+    description: Optional[str]
+
+
+class ConditionVector:
+    def __init__(self, vals: dict[str, dict[str, VALUE_TYPE]]):
+        self.values: list[ConditionScalar] = []
+        for name, val_dict in vals.items():
+            value = val_dict.get('value', 0.0)
+            unc = val_dict.get('uncertainty', None)
+            desc = val_dict.get('description', name)
+            self.values.append(ConditionScalar(name=name, value=value, uncertainty=unc, description=desc))
 
 
 @dataclass
-class Scalar:
-    # A scalar value for a condition
-    _name: str
-    _value: SCALAR_TYPE
-    _unit: Optional[str]
-    _uncertainty: Optional[VALUE_TYPE]
-    _desired_unit: Optional[str]
+class ConditionUnit(ConditionScalar):
+    unit: str
 
     def __init__(self, **kwargs):
         # Data class automatically assigns class values
         super().__init__(**kwargs)
-        self._converter = Converter(self._unit)
-        self._desired_unit = self._unit
+        self._converter = Converter(self.unit)
+        self._desired_unit: str = self.unit
 
     def convert(self, new_unit: str):
-        if self.are_units_sensible(new_unit):
+        if self._converter.are_units_sensible(new_unit):
             self._desired_unit = new_unit
-            self._converter(self._value, new_unit)
+            self._converter(self.value, new_unit)
         else:
-            # TODO: warn the user in a meaningful way (no Exceptions!)
+            # TODO: warn the user in a meaningful way, but do not throw an Exception
             pass
-
-    def are_units_sensible(self, units: str):
-        """A check to see if the units passed to the method make sense based on the condition type used."""
-        if self._converter:
-            compatible = self._converter.get_compatible_units()
-            std_units = standardize_units(units)
-            if len(compatible) == len(std_units):
-                for comp, unit in zip(compatible, std_units):
-                    if unit not in comp:
-                        return False
-            else:
-                return False
-        return True
-
-
-class Vector:
-    def __init__(self, vals: dict[str, dict[str, VALUE_TYPE]]):
-        self.values: list[Scalar] = []
-        for name, val_dict in vals.items():
-            value = val_dict.get('value', 0.0)
-            unit = val_dict.get('unit', None)
-            unc = val_dict.get('uncertainty', None)
-            self.values.append(Scalar(name=name, value=value, unit=unit, uncertainty=unc))
