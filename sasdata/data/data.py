d8192680dea7c43506b892e5aeafc2a2bbf3c23b
krzywon <krzywon@yahoo.com>
2024-03-22 15:55:22 -0400

    Add conditions to DataMixin

--- sasdata/data/data.py
+++ sasdata/data/data.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-
 import numpy as np
+from sasdata.condition.condition_meta import ConditionVector
 
 
 @dataclass
@@ -8,10 +8,13 @@ class DataMixin:
     """Class for storing as-loaded data from any source."""
     # An array of dependent values (n-dimensional)
     dependent: np.ndarray
-    # An array of signal values
+    # An array of resolutions (known uncertainties) for the dependent variables
+    resolution: np.ndarray
+    # An array of (systematic) uncertainties for the dependent variables
+    uncertainties: np.ndarray
+    # An array of signal values (m datasets, each n-dimensional)
     signal: np.ndarray
     # An array of uncertainties for the signal values where np.shape(noise) == np.shape(signal)
     noise: np.ndarray
-    # An array of uncertainties for the dependent variables
-    resolution: np.ndarray
-    # Plottable dependent values
+    # A series of external conditions applied during the measurement that resulted in this data set.
+    conditions: ConditionVector
