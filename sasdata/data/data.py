11099cf735762eea39ef3669b7381d677ea254f3
krzywon <krzywon@yahoo.com>
2024-06-06 15:51:17 -0400

    Document what is still required in a TODO

--- sasdata/data/data.py
+++ sasdata/data/data.py
@@ -3,6 +3,17 @@ import numpy as np
 from sasdata.condition.condition_meta import ConditionVector
 
 
+# TODO: The following items are still needed:
+#   - DataMixin.id: A unique id that can be used to map Data into Trends and Views to Data
+#   - DataMixin.views: An object that holds the direct child Views associated with the Data
+#   - Differentiate as-loaded data from plottable data (this would not be a datclass, just a separate class)
+#   - Create the 'View' class - This could be a subclass of the plottable data - that holds transforms of the Data
+#       - should subclass DataMixin and have an attribute that holds the DataMixin.id value the View was generated from
+#   - Incorporate the resolution functions (maybe post-1.0?)
+#   - Create a data manager that lives in this package that holds all Data, Views, and Trends (DataModel?)
+#   - Convert Reader and Writer classes to Import and Export classes
+#       - Convert them all to use the newest Data class
+#   - Define specific Condition subclasses for often-used sample environments and sample conditions (maybe post-1.0?)
 @dataclass
 class DataMixin:
     """Class for storing as-loaded data from any source."""
