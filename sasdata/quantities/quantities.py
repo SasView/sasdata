cb0ac175b6d3d77925aabbe0c538c2f50cef933e
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-08-05 09:14:59 +0100

    Some units

--- sasdata/quantities/quantities.py
+++ sasdata/quantities/quantities.py
@@ -1,6 +1,9 @@
 from typing import Collection, Sequence, TypeVar, Generic
 from dataclasses import dataclass
 
+from numpy._typing import ArrayLike
+
+
 class Dimensions:
     """
 
@@ -58,6 +61,17 @@ class Dimensions:
             self.current * power,
             self.temperature * power)
 
+    def __eq__(self, other: "Dimensions"):
+        if isinstance(other, Dimensions):
+            return (self.length == other.length and
+                    self.time == other.time and
+                    self.mass == other.mass and
+                    self.current == other.current and
+                    self.temperature == other.temperature)
+
+        return NotImplemented
+
+
 
 @dataclass
 class UnitName:
@@ -102,15 +116,46 @@ class Unit:
 
         return Unit(self.scale**power, self.dimensions**power)
 
+    def equivalent(self, other: "Unit"):
+        return self.dimensions == other.dimensions
 
-QuantityType = TypeVar("QuantityType")
-class Quantity(Generic[QuantityType]):
+
+# QuantityType = TypeVar("QuantityType")
+class Quantity[QuantityType]:
     def __init__(self, value: QuantityType, units: Unit):
         self.value = value
         self.units = units
 
     def in_units_of(self, units: Unit) -> QuantityType:
         pass
+
+    def __mul__(self, other: ArrayLike | "Quantity" ) -> "Quantity":
+        if isinstance(other, Quantity):
+            pass
+
+        else:
+            pass
+
+    def __truediv__(self, other: float | "Quantity") -> "Quantity":
+        if isinstance(other, Quantity):
+            pass
+
+        else:
+            pass
+
+    def __rdiv__(self, other: float | "Quantity") -> "Quantity":
+        if isinstance(other, Quantity):
+            pass
+
+        else:
+            pass
+    def __add__(self, other: "Quantity") -> "Quantity":
+        if isinstance(other, Quantity):
+            pass
+
+    def __sub__(self, other: "Quantity") -> "Quantity":
+        if isinstance(other, Quantity):
+            pass
 
 class ExpressionMethod:
     pass
