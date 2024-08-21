29bed57d7399db2e3ba707ca3ca29e81c8303066
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-08-21 10:38:51 +0100

    Work towards new data object

--- sasdata/data.py
+++ sasdata/data.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from quantities.quantity import Quantity, NamedQuantity
-from sasdata.metadata import MetaData
+from sasdata.quantities.quantity import Quantity, NamedQuantity
+from sasdata.metadata import Metadata
 
 import numpy as np
 
@@ -19,84 +19,11 @@ class SasData:
         self._raw_metadata = raw_metadata
         self._verbose = verbose
 
-        self.metadata = Metadata(AccessorTarget(raw_metadata, verbose=verbose), instrument)
+@dataclass
+class DataSet:
+    abscissae: list[NamedQuantity[np.ndarray]]
+    ordinate: NamedQuantity[np.ndarray]
+    other: list[NamedQuantity[np.ndarray]]
 
-        # Components that need to be organised after creation
-        self.mask = None # TODO: fill out
-        self.model_requirements = None # TODO: fill out
-
-    #TODO: This seems oriented around 1D I vs Q data. What about 2D data?
-    @property
-    def ordinate() -> Quantity:
-        raise NotImplementedError()
-
-    @property
-    def abscissae(self) -> Quantity:
-        if self.dataset_type == one_dim:
-            return self._data_contents['Q']
-        elif self.dataset_type == two_dim:
-            # Type hinting is a bit lacking. Assume each part of the zip is a scalar value.
-            data_contents = zip(self._data_contents['Qx'].value, self._data_contents['Qy'].value)
-            # Use this value to extract units etc. Assume they will be the same for Qy.
-            reference_data_content = self._data_contents['Qx']
-            # TODO: If this is a derived quantity then we are going to lose that
-            # information.
-            #
-            # TODO: Won't work when there's errors involved. On reflection, we
-            # probably want to avoid creating a new Quantity but at the moment I
-            # can't see a way around it.
-            return Quantity(data_contents, reference_data_content.units)
-        return None
-
-    def __getitem__(self, item: str):
-        return self._data_contents[item]
-
-    def summary(self, indent = "  ", include_raw=False):
-        s = f"{self.name}\n"
-
-        for data in self._data_contents:
-            s += f"{indent}{data}\n"
-
-        s += f"Metadata:\n"
-        s += "\n"
-        s += self.metadata.summary()
-
-        if include_raw:
-            s += key_tree(self._raw_metadata)
-
-        return s
-
-    @staticmethod
-    def deserialise(data: str) -> "SasData":
-        json_data = json.loads(data)
-        return SasData.deserialise_json(json_data)
-
-    @staticmethod
-    def deserialise_json(json_data: dict) -> "SasData":
-        name = json_data["name"]
-        data_contents = {}
-        dataset_type = json_data["dataset_type"] # TODO: update when DatasetType is more finalized
-        metadata = json_data["metadata"].deserialise_json()
-        for quantity in json_data["data_contents"]:
-            data_contents[quantity["label"]] = Quantity.deserialise_json(quantity)
-        return SasData(name, data_contents, dataset_type, metadata)
-
-    def serialise(self) -> str:
-        return json.dumps(self._serialise_json())
-
-    # TODO: fix serializers eventually
-    def _serialise_json(self) -> dict[str, Any]:
-        data = []
-        for d in self._data_contents:
-            quantity = self._data_contents[d]
-            quantity["label"] = d
-            data.append(quantity)
-        return {
-            "name": self.name,
-            "data_contents": data,
-            "dataset_type": None, # TODO: update when DatasetType is more finalized
-            "verbose": self._verbose,
-            "metadata": self.metadata.serialise_json(),
-            "mask": {},
-            "model_requirements": {}
-        }
+    metadata: Metadata
+    model_requirements: ModellingRequirements
--- sasdata/temp_hdf5_reader.py
+++ sasdata/temp_hdf5_reader.py
@@ -11,180 +11,50 @@ from h5py._hl.dataset import Dataset as HDF5Dataset
 from h5py._hl.group import Group as HDF5Group
 
 
-from sasdata.data import SasData
-from sasdata.data_backing import Dataset as SASDataDataset, Group as SASDataGroup
-from sasdata.metadata import Instrument, Collimation, Aperture, Source
-from sasdata.quantities.accessors import AccessorTarget
+from sasdata.data import DataSet
+from sasdata.raw_form import RawData
+from sasdata.raw_form import Dataset as SASDataDataset, Group as SASDataGroup
 
-from sasdata.quantities.quantity import NamedQuantity
-from sasdata.quantities import units
-from sasdata.quantities.unit_parser import parse
-
-# test_file = "./example_data/1d_data/33837rear_1D_1.75_16.5_NXcanSAS_v3.h5"
-# test_file = "./example_data/1d_data/33837rear_1D_1.75_16.5_NXcanSAS.h5"
-# test_file = "./example_data/2d_data/BAM_2D.h5"
-test_file = "./example_data/2d_data/14250_2D_NoDetInfo_NXcanSAS_v3.h5"
-# test_file = "./example_data/2d_data/33837rear_2D_1.75_16.5_NXcanSAS_v3.h5"
+test_file = "./example_data/1d_data/33837rear_1D_1.75_16.5_NXcanSAS_v3.h5"
+test_file = "./example_data/1d_data/33837rear_1D_1.75_16.5_NXcanSAS.h5"
 
 logger = logging.getLogger(__name__)
 
+def hdf5_attr(entry):
+    return entry
 
 def recurse_hdf5(hdf5_entry):
     if isinstance(hdf5_entry, HDF5Dataset):
 
-        attributes = {name: hdf5_entry.attrs[name] for name in hdf5_entry.attrs}
-
         if isinstance(hdf5_entry.dtype, np.dtypes.BytesDType):
             data = hdf5_entry[()][0].decode("utf-8")
-
-            return SASDataDataset[str](
-                name=hdf5_entry.name, data=data, attributes=attributes
-            )
-
         else:
             data = np.array(hdf5_entry, dtype=hdf5_entry.dtype)
 
-            return SASDataDataset[np.ndarray](
-                name=hdf5_entry.name, data=data, attributes=attributes
-            )
+        attributes = {name: hdf5_attr(hdf5_entry.attrs[name]) for name in hdf5_entry.attrs}
+
+        return SASDataDataset(
+            name=hdf5_entry.name,
+            data=data,
+            attributes=attributes)
 
     elif isinstance(hdf5_entry, HDF5Group):
         return SASDataGroup(
             name=hdf5_entry.name,
-            children={key: recurse_hdf5(hdf5_entry[key]) for key in hdf5_entry.keys()},
-        )
+            children={key: recurse_hdf5(hdf5_entry[key]) for key in hdf5_entry.keys()})
 
     else:
-        raise TypeError(
-            f"Unknown type found during HDF5 parsing: {type(hdf5_entry)} ({hdf5_entry})"
-        )
+        raise TypeError(f"Unknown type found during HDF5 parsing: {type(hdf5_entry)} ({hdf5_entry})")
 
+def load_data(filename) -> list[RawData]:
+    with h5py.File(filename, 'r') as f:
 
-GET_UNITS_FROM_ELSEWHERE = units.meters
-
-
-def connected_data(node: SASDataGroup, name_prefix="") -> list[NamedQuantity]:
-    """In the context of NeXus files, load a group of data entries that are organised together
-    match up the units and errors with their values"""
-    # Gather together data with its error terms
-
-    uncertainty_map = {}
-    uncertainties = set()
-    entries = {}
-
-    for name in node.children:
-        child = node.children[name]
-
-        if "units" in child.attributes:
-            units = parse(child.attributes["units"])
-        else:
-            units = GET_UNITS_FROM_ELSEWHERE
-
-        quantity = NamedQuantity(
-            name=name_prefix + child.name, value=child.data, units=units
-        )
-
-        # Turns out people can't be trusted to use the same keys here
-        if "uncertainty" in child.attributes or "uncertainties" in child.attributes:
-            try:
-                uncertainty_name = child.attributes["uncertainty"]
-            except:
-                uncertainty_name = child.attributes["uncertainties"]
-            uncertainty_map[name] = uncertainty_name
-            uncertainties.add(uncertainty_name)
-
-        entries[name] = quantity
-
-    output = []
-
-    for name, entry in entries.items():
-        if name not in uncertainties:
-            if name in uncertainty_map:
-                uncertainty = entries[uncertainty_map[name]]
-                new_entry = entry.with_standard_error(uncertainty)
-                output.append(new_entry)
-            else:
-                output.append(entry)
-
-    return output
-
-
-def parse_apertures(node) -> list[Aperture]:
-    result = []
-    aps = [a for a in node if "aperture" in a]
-    for ap in aps:
-        distance = None
-        size = None
-        if "distance" in node[ap]:
-            distance = node[ap]["distance"]
-        if "size" in node[ap]:
-            x = y = z = None
-            if "x" in node[ap]:
-                x = node[ap]["size"]["x"]
-            if "y" in node[ap]:
-                y = node[ap]["size"]["y"]
-            if "z" in node[ap]:
-                z = node[ap]["size"]["z"]
-            if x is not None or y is not None or z is not None:
-                size = (x, y, z)
-        result.append(Aperture(distance=distance, size=size, size_name=size_name, name=name, apType=apType))
-    return result
-
-
-def parse_source(node) -> Source:
-    beam_shape = None
-    beam_size = None
-    wavelength = None
-    wavelength_min = None
-    wavelength_max = None
-    wavelength_spread = None
-    if "beam_shape" in node:
-        beam_shape = node["beam_shape"]
-    if "wavelength" in node:
-        wavelength = node["wavelength"]
-    if "wavelength_min" in node:
-        wavelength = node["wavelength_min"]
-    if "wavelength_max" in node:
-        wavelength = node["wavelength_max"]
-    if "wavelength_spread" in node:
-        wavelength = node["wavelength_spread"]
-    return Source(
-        radiation=node["radiation"].asstr()[0],
-        beam_shape=beam_shape,
-        beam_size=beam_size,
-        wavelength=wavelength,
-        wavelength_min=wavelength_min,
-        wavelength_max=wavelength_max,
-        wavelength_spread=wavelength_spread,
-    )
-
-
-def parse_collimation(node) -> Collimation:
-    if "length" in node:
-        length = node["length"]
-    else:
-        length = None
-    return Collimation(length=length, apertures=parse_apertures(node))
-
-
-def parse_instrument(raw, node) -> Instrument:
-    collimations = [
-        parse_collimation(node[x])
-        for x in node
-        if "collimation" in x
-    ]
-    return Instrument(
-        collimations= [parse_collimation(node[x]) for x in node if "collimation" in x],
-        detector=[parse_detector(node[d]) for d in node if "detector" in d],
-        source=parse_source(node["sassource"]),
-    )
-
-
-def load_data(filename) -> list[SasData]:
-    with h5py.File(filename, "r") as f:
-        loaded_data: list[SasData] = []
+        loaded_data: list[RawData] = []
 
         for root_key in f.keys():
+
+            print(root_key)
+
             entry = f[root_key]
 
             data_contents = []
@@ -192,44 +62,30 @@ def load_data(filename) -> list[SasData]:
 
             entry_keys = [key for key in entry.keys()]
 
-            if "sasdata" not in entry_keys and "data" not in entry_keys:
-                logger.warning("No sasdata or data key")
+            if "sasdata" not in entry_keys:
+                logger.warning("")
 
             for key in entry_keys:
                 component = entry[key]
-                lower_key = key.lower()
-                if lower_key == "sasdata" or lower_key == "data":
-                    datum = recurse_hdf5(component)
-                    # TODO: Use named identifier
-                    data_contents = connected_data(datum, "FILE_ID_HERE")
+                if key.lower() == "sasdata":
+                    print("found sasdata, skipping for now")
 
                 else:
                     raw_metadata[key] = recurse_hdf5(component)
 
-            instrument = opt_parse(f["sasentry01"], "sasinstrument", parse_instrument)
-            sample = opt_parse(f["sasentry01"], "sassample", parse_sample)
-            process = [parse_process(f["sasentry01"][p]) for p in f["sasentry01"] if "sasprocess" in p]
-            title = opt_parse(f["sasentry01"], "title", parse_string)
-            run = [parse_string(f["sasentry01"][r]) for r in f["sasentry01"] if "run" in r]
-            definition = opt_parse(f["sasentry01"], "definition", parse_string)
-
-            metadata = Metadata(process=process, instrument=instrument, sample=sample, title=title, run=run, definition=definition)
 
             loaded_data.append(
-                SasData(
+                RawData(
                     name=root_key,
                     data_contents=data_contents,
-                    raw_metadata=SASDataGroup("root", raw_metadata),
-                    instrument=instrument,
-                    verbose=False,
-                )
-            )
+                    raw_metadata=raw_metadata))
 
         return loaded_data
 
 
-if __name__ == "__main__":
-    data = load_data(test_file)
 
-    for dataset in data:
-        print(dataset.summary(include_raw=False))
+
+data = load_data(test_file)
+
+for dataset in data:
+    print(dataset)
