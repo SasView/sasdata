e36189acedd9a67468d6f239ddac0729ace1d056
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-09-11 09:23:26 +0100

    Pass in the dimensions so this code is correct.

--- sasdata/quantities/unit_parser.py
+++ sasdata/quantities/unit_parser.py
@@ -1,6 +1,6 @@
-from sasdata.quantities.units import NamedUnit
+from sasdata.quantities.units import Dimensions, NamedUnit
 
 
 def parse_unit(unit_str: str) -> NamedUnit:
     # TODO: Not implemented. This is just to enable testing.
-    return NamedUnit()
+    return NamedUnit(1, Dimensions())
