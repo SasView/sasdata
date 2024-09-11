d03db7171bf44e20b42407f8f7f3f088b68e85c0
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-09-11 15:49:17 +0100

    Found a regex for splitting up the string.

--- sasdata/quantities/unit_parser.py
+++ sasdata/quantities/unit_parser.py
@@ -1,5 +1,8 @@
 from sasdata.quantities.units import Dimensions, NamedUnit
+from re import findall
 
+def split_unit_str(unit_str: str) -> list[str]:
+    return findall(r'[A-Za-z]+|[-\d]+', unit_str)
 
 def parse_unit(unit_str: str) -> NamedUnit:
     # TODO: Not implemented. This is just to enable testing.
