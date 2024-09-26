b58e973691dd9feda2f62ea6b5996bc291264af9
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-09-26 10:21:33 +0100

    Quantities ready for testing

--- sasdata/quantities/quantity_examples.py
+++ sasdata/quantities/quantity_examples.py
@@ -1,8 +1,9 @@
+from sasdata.quantities.quantity import Quantity
 from sasdata.quantities import units
-from sasdata.quantities.quantity import NamedQuantity
 
-x = NamedQuantity("x", 1, units.meters, standard_error=1)
-y = NamedQuantity("y", 1, units.decimeters, standard_error=1)
+x = Quantity(1, units.meters, variance=1)
+y = Quantity(1, units.meters, variance=1)
 
-print(x+y)
-print((x+y).to_units_of(units.centimeters))
+z = x+y
+
+print(z)
