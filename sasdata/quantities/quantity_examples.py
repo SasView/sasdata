a90a92dd2e9fe8814b71e825ad1c67192d474bd3
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-09-27 16:46:49 +0100

    Fixed error in helper function

--- sasdata/quantities/quantity_examples.py
+++ sasdata/quantities/quantity_examples.py
@@ -1,8 +1,9 @@
 from sasdata.quantities.quantity import Quantity, NamedQuantity
 from sasdata.quantities import units
 
-x = NamedQuantity("x", 1, units.meters, variance=1)
-y = NamedQuantity("y", 1, units.meters, variance=1)
+x = NamedQuantity("x", 1, units.meters, standard_error=1)
+y = NamedQuantity("y", 1, units.decimeters, standard_error=1)
 
 print(x+y)
 print(x+x)
+print(y+y)
