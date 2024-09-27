d3fad255cae905fb5deb56809d662f6452806edf
lucas-wilkins <lucas.wilkins@stfc.ac.uk>
2024-09-27 17:09:04 +0100

    Fixed error formatting bug

--- sasdata/quantities/quantity_examples.py
+++ sasdata/quantities/quantity_examples.py
@@ -5,5 +5,4 @@ x = NamedQuantity("x", 1, units.meters, standard_error=1)
 y = NamedQuantity("y", 1, units.decimeters, standard_error=1)
 
 print(x+y)
-print(x+x)
-print(y+y)
+print((x+y).to_units_of(units.centimeters))
