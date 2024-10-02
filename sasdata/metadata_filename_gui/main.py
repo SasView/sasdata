71660d3a32cf7cc1e3d7efe82b08029419a5f228
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-10-02 15:44:57 +0100

    Start playing around with layout.

--- sasdata/metadata_filename_gui/main.py
+++ sasdata/metadata_filename_gui/main.py
@@ -1,8 +1,23 @@
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtWidgets import QWidget, QApplication, QVBoxLayout, QLineEdit, QHBoxLayout, QLabel
 
 class MetadataFilenameDialog(QWidget):
-    def __init__(filename: str):
+    def __init__(self, filename: str):
+        super().__init__()
+
         self.filename = filename
+
+        self.filename_line_label = QLabel(f'Filename: <b>{filename}</b>')
+
+        self.seperator_chars_label = QLabel('Seperators')
+        self.separator_chars = QLineEdit()
+
+        self.filename_separator_layout = QHBoxLayout()
+        self.filename_separator_layout.addWidget(self.filename_line_label)
+        self.filename_separator_layout.addWidget(self.seperator_chars_label)
+        self.filename_separator_layout.addWidget(self.separator_chars)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addLayout(self.filename_separator_layout)
 
 
 if __name__ == "__main__":
