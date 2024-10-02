b6ee8027f1743df04da25d0f49f2d6346d67879a
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-10-02 15:11:19 +0100

    Start creating the dialog.

---
+++ sasdata/metadata_filename_gui/main.py
@@ -0,0 +1,15 @@
+from PySide6.QtWidgets import QWidget, QApplication
+
+class MetadataFilenameDialog(QWidget):
+    def __init__():
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    widget = MetadataFilenameDialog()
+    widget.show()
+
+
+    exit(app.exec())
