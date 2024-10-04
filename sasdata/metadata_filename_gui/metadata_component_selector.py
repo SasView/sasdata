4c6a7d31f8759223694a4a97ff77ad84db7ef2c8
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-10-04 09:39:32 +0100

    Draw option on screen where there are new options.

--- sasdata/metadata_filename_gui/metadata_component_selector.py
+++ sasdata/metadata_filename_gui/metadata_component_selector.py
@@ -1,6 +1,22 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtWidgets import QWidget, QPushButton, QHBoxLayout
 
 class MetadataComponentSelector(QWidget):
     def __init__(self):
         super().__init__()
-        # TODO: Implement
+        self.options: list[str]
+        self.option_buttons: list[QPushButton]
+        self.layout = QHBoxLayout()
+
+    def clear_options(self):
+        for i in reversed(range(self.layout.count() - 1)):
+            self.layout.takeAt(i).widget().deleteLater()
+
+    def draw_options(self, new_options: list[str]):
+        self.clear_options()
+        self.options = new_options
+        self.option_buttons = []
+        for option in self.options:
+            option_button = QPushButton(option)
+            option_button.setCheckable(True)
+            self.layout.addWidget(option_button)
+            self.option_buttons.append(option_button)
