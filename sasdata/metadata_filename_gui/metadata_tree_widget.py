e96d68c5da3fdaf011f7e42811e9fbe2bce767c4
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-10-04 09:25:56 +0100

    Created a separate widget for the tree.

---
+++ sasdata/metadata_filename_gui/metadata_tree_widget.py
@@ -0,0 +1,20 @@
+from PySide6.QtWidgets import QTreeWidget, QTreeWidgetItem
+from sasdata.metadata_filename_gui.metadata_component_selector import MetadataComponentSelector
+
+class MetadataTreeWidget(QTreeWidget):
+    def __init__(self):
+        super().__init__()
+        self.setColumnCount(2)
+        self.setHeaderLabels(['Name', 'Filename Components'])
+
+        # TODO: This is placeholder data that'll need to be replaced by the real metadata.
+
+        # FIXME: This won't update.
+        metadata = {'Instrument': ['Slit width', 'Other']}
+        for top_level, items in metadata.items():
+            top_level_item = QTreeWidgetItem([top_level])
+            for metadatum in items:
+                selector = MetadataComponentSelector()
+                metadatum_item = QTreeWidgetItem([metadatum, selector])
+                top_level_item.addChild(metadatum_item)
+            self.insertTopLevelItem(top_level_item)
