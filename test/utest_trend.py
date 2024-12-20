bfeed18ba1f90c46c846b223a83a92485e9b33c1
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2024-12-20 15:52:15 +0000

    Wrote a test for the trend.

--- test/utest_trend.py
+++ test/utest_trend.py
@@ -1,37 +1,25 @@
-from os import listdir, path
-
 import pytest
-
-import sasdata.temp_ascii_reader as ascii_reader
-from sasdata.ascii_reader_metadata import AsciiMetadataCategory
-from sasdata.quantities.units import per_angstrom, per_nanometer
+from os import path, listdir
+from sasdata.ascii_reader_metadata import AsciiMetadataCategory, AsciiReaderMetadata
+from sasdata.quantities.units import per_nanometer
 from sasdata.temp_ascii_reader import AsciiReaderParams
+import sasdata.temp_ascii_reader as ascii_reader
 from sasdata.trend import Trend
 
-mumag_test_directories = [
+test_directories = [
     'FeNiB_perpendicular_Bersweiler_et_al',
     'Nanoperm_perpendicular_Honecker_et_al',
     'NdFeB_parallel_Bick_et_al'
 ]
 
-custom_test_directory = 'custom_test'
+@pytest.mark.parametrize('directory_name', test_directories)
+def test_trend_build(directory_name: str):
+    """Try to build a trend object on the MuMag datasets, and see if all the Q items match (as they should)."""
+    load_from = path.join(path.dirname(__file__), directory_name)
+    files_to_load = listdir(load_from)
 
-def get_files_to_load(directory_name: str) -> list[str]:
-    load_from = path.join(path.dirname(__file__), 'trend_test_data', directory_name)
-    base_filenames_to_load = listdir(load_from)
-    files_to_load = [path.join(load_from, basename) for basename in base_filenames_to_load]
-    return files_to_load
-
-@pytest.mark.parametrize('directory_name', mumag_test_directories)
-def test_trend_build_interpolate(directory_name: str):
-    """Try to build a trend object on the MuMag datasets"""
-    files_to_load = get_files_to_load(directory_name)
-    params = AsciiReaderParams(
-        filenames=files_to_load,
-        columns=[('Q', per_nanometer), ('I', per_nanometer), ('dI', per_nanometer)],
-    )
-    params.separator_dict['Whitespace'] = True
-    params.metadata.master_metadata['magnetic'] = AsciiMetadataCategory(
+    metadata = AsciiReaderMetadata()
+    metadata.master_metadata['magnetic'] = AsciiMetadataCategory(
         values={
             'counting_index': 0,
             'applied_magnetic_field': 1,
@@ -39,31 +27,18 @@ def test_trend_build_interpolate(directory_name: str):
             'demagnetizing_field': 3
         }
     )
+
+    params = AsciiReaderParams(
+        filenames=files_to_load,
+        starting_line=0,
+        columns=[('Q', per_nanometer), ('I', per_nanometer), ('dI', per_nanometer)],
+        excluded_lines=set(),
+        separator_dict={'Whitespace': True, 'Comma': False, 'Tab': False},
+        metadata=metadata,
+    )
     data = ascii_reader.load_data(params)
     trend = Trend(
         data=data,
         trend_axis=['magnetic', 'applied_magnetic_field']
-    )
-    # Initially, the q axes in this date don't exactly match
-    to_interpolate_on = 'Q'
-    assert not trend.all_axis_match(to_interpolate_on)
-    interpolated_trend = trend.interpolate(to_interpolate_on)
-    assert interpolated_trend.all_axis_match(to_interpolate_on)
-
-def test_trend_q_axis_match():
-    files_to_load = get_files_to_load(custom_test_directory)
-    params = AsciiReaderParams(
-        filenames=files_to_load,
-        columns=[('Q', per_angstrom), ('I', per_angstrom)]
-    )
-    params.metadata.master_metadata['magnetic'] = AsciiMetadataCategory(
-        values={
-            'counting_index': 0,
-        }
-    )
-    data = ascii_reader.load_data(params)
-    trend = Trend(
-        data=data,
-        trend_axis=['magnetic', 'counting_index']
     )
     assert trend.all_axis_match('Q')
