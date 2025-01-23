87d219929ebbc9d40b501529923179139e54016b
summerhenson <summerh@gmail.com>
2025-01-23 14:30:32 -0500

    Create urls for database.

--- sasdata/fair_database/data/views.py
+++ sasdata/fair_database/data/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 # Create your views here
 from django.http import HttpResponse
 
-def list_data(request):
+def list_data(request, username = None):
     return HttpResponse("Hello World! This is going to display data later.")
+
+def data_info(request, db_id):
+    return HttpResponse("This is going to allow viewing data file %s." % db_id)
+
+def upload(request, db_id = None):
+    return HttpResponse("This is going to allow data uploads.")
+
+def download(request, data_id):
+    return HttpResponse("This is going to allow downloads of data %s." % data_id)
