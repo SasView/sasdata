7902e551d203cf36893026305e065ccd95d5ea9f
summerhenson <summerh@gmail.com>
2025-01-23 16:16:45 -0500

    View for specific data

--- sasdata/fair_database/data/views.py
+++ sasdata/fair_database/data/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # Create your views here
 from django.http import HttpResponse, HttpResponseBadRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from sasdata.dataloader.loader import Loader
 from .models import Data
 
 @api_view(['GET'])
@@ -26,8 +28,24 @@ def list_data(request, username = None):
         return Response(data_list)
     return HttpResponseBadRequest("not get method")
 
+@api_view(['GET'])
 def data_info(request, db_id):
-    return HttpResponse("This is going to allow viewing data file %s." % db_id)
+    if request.method == 'GET':
+        loader = Loader()
+        data_db = get_object_or_404(Data, id=db_id)
+        if data_db.is_public:
+            data_list = loader.load(data_db.file.path)
+            contents = [str(data) for data in data_list]
+            return_data = {data_db.file_name: contents}
+        # rewrite with "user.is_authenticated"
+        elif (data_db.current_user == request.user) and request.user.is_authenticated:
+            data_list = loader.load(data_db.file.path)
+            contents = [str(data) for data in data_list]
+            return_data = {data_db.file_name: contents}
+        else:
+            return HttpResponseBadRequest("Database is either not public or wrong auth token")
+        return Response(return_data)
+    return HttpResponseBadRequest()
 
 def upload(request, db_id = None):
     return HttpResponse("This is going to allow data uploads.")
