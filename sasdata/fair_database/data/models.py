0e272a5fbaf57f9ce40c4c786f6460f8e0e34325
summerhenson <summerh@gmail.com>
2025-01-23 13:31:55 -0500

    Add Data model class from webfit

--- sasdata/fair_database/data/models.py
+++ sasdata/fair_database/data/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
 
 # Create your models here.
+class Data(models.Model):
+    #username
+    current_user = models.ForeignKey(User, blank=True,
+                                     null=True, on_delete=models.CASCADE)
+
+    #file name
+    file_name = models.CharField(max_length=200, default=None,
+                                 blank=True, null=True, help_text="File name")
+
+    #imported data
+    #user can either import a file path or actual file
+    file = models.FileField(blank=False, default=None, help_text="This is a file",
+                            upload_to="uploaded_files", storage=FileSystemStorage())
+
+    #is the data public?
+    is_public = models.BooleanField(default=False,
+                                    help_text= "opt in to submit your data into example pool")
