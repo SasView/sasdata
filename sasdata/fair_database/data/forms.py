38d2371c146407319018e367f4f4eefc495ae5c2
summerhenson <summerh@gmail.com>
2025-01-24 15:18:19 -0500

    Enable file upload

---
+++ sasdata/fair_database/data/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Data
+
+# Create the form class.
+class DataForm(forms.ModelForm):
+    class Meta:
+        model = Data
+        fields = ["file", "is_public"]
