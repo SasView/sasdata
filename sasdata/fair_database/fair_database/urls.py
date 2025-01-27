486e9aaef155fdfc8ed240f5f4965f15203886bf
summerhenson <summerh@gmail.com>
2025-01-27 10:43:17 -0500

    Add version to url

--- sasdata/fair_database/fair_database/urls.py
+++ sasdata/fair_database/fair_database/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
-    path('data/', include("data.urls")),
-    path('admin/', admin.site.urls),
+    re_path(r"^(?P<version>(v1))/data/", include("data.urls")),
+    path("admin/", admin.site.urls),
 ]
