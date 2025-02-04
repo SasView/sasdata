4810d5fe11fc54866d150576c879a7151556dbfd
summerhenson <summerh@gmail.com>
2025-02-04 15:54:43 -0500

    Tests for logout

--- sasdata/fair_database/user_app/tests.py
+++ sasdata/fair_database/user_app/tests.py
@@ -2,23 +2,58 @@ import requests
 
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import RequestsClient
+from rest_framework.test import APIClient
+
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class AuthTests(TestCase):
 
-    def setup(self):
-        self.client = RequestsClient()
+    def setUp(self):
+        self.client = APIClient()
+        self.register_data = {
+            "email": "email@domain.org",
+            "username": "testUser",
+            "password1": "sasview!",
+            "password2": "sasview!"
+        }
+        self.login_data = {
+            "username": "testUser",
+            "email": "email@domain.org",
+            "password": "sasview!"
+        }
 
     def test_register(self):
-        data = {
-            'email': 'test@test.com',
-            'username': 'testUser',
-            'password': 'testPassword'
-        }
-        response = self.client.post('/_allauth/app/v1/auth/signup',data=data)
-        print(response.content)
+        response = self.client.post('/dj-rest-auth/registration/',data=self.register_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = User.objects.get(username="testUser")
+        self.assertEquals(user.email, self.register_data["email"])
+
+    def test_login(self):
+        user = User.objects.create_user(username="testUser", password="sasview!", email="email@domain.org")
+        response = self.client.post('/dj-rest-auth/login', data=self.login_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_logout(self):
+        user = User.objects.create_user(username="testUser", password="sasview!", email="email@domain.org")
+        self.client.post('/dj-rest-auth/login', data=self.login_data)
+        response = self.client.post('/dj-rest-auth/logout')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content, b'{"detail":"Successfully logged out."}')
+
+    def test_register_logout(self):
+        self.client.post('/dj-rest-auth/registration/', data=self.register_data)
+        response = self.client.post('/dj-rest-auth/logout')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content, b'{"detail":"Successfully logged out."}')
+
+    def test_register_login(self):
+        register_response = self.client.post('/dj-rest-auth/registration/', data=self.register_data)
+        logout_response = self.client.post('/dj-rest-auth/logout')
+        login_response = self.client.post('/dj-rest-auth/login', data=self.login_data)
+        self.assertEqual(register_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(logout_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(login_response.status_code, status.HTTP_200_OK)
 
 #can register a user, user is w/in User model
 # user is logged in after registration
