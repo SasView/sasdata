8ecc9b5cc245929dec43ec1ff25f2d8f62855230
summerhenson <summerh@gmail.com>
2025-02-07 16:40:24 -0500

    Require private data have an owner

--- sasdata/fair_database/data/serializers.py
+++ sasdata/fair_database/data/serializers.py
@@ -6,3 +6,9 @@ class DataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Data
         fields = "__all__"
+
+    def validate(self, data):
+        print(data)
+        if not data['is_public'] and not data['current_user']:
+            raise serializers.ValidationError('private data must have an owner')
+        return data
