a46cb1afef3efb11d9ab509e1736e1db12301ef9
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-02-17 10:22:00 +0000

    Uncertainties should have the same units.

--- sasdata/default_units.py
+++ sasdata/default_units.py
@@ -6,11 +6,16 @@ from sasdata.dataset_types import unit_kinds
 
 default_units = {
     'Q': [unit.per_nanometer, unit.per_angstrom, unit.per_meter],
-    'I': [unit.per_centimeter, unit.per_meter]
+    'I': [unit.per_centimeter, unit.per_meter],
+    'dQ': 'Q',
+    'dI': 'I'
 }
 
 def defaults_or_fallback(column_name: str) -> list[NamedUnit]:
-    return default_units.get(column_name, unit_kinds[column_name].units)
+    value = default_units.get(column_name, unit_kinds[column_name].units)
+    if isinstance(value, str):
+        return defaults_or_fallback(value)
+    return value
 
 def first_default_for_fallback(column_name: str) -> NamedUnit:
     return defaults_or_fallback(column_name)[0]
