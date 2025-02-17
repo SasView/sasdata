70cfd9e08979c1a5388a0c899ff66130cd66b63d
Adam Washington <adam.washington@stfc.ac.uk>
2025-02-17 13:19:26 +0000

    Add latex symbols to units which need them

--- sasdata/quantities/_build_tables.py
+++ sasdata/quantities/_build_tables.py
@@ -3,85 +3,89 @@ Builds a data file containing details of units
 """
 
 import numpy as np
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from _units_base import Dimensions, Unit
 from _autogen_warning import warning_text
 
-bigger_magnitudes = [
-    ("E", None, "exa", 1e18),
-    ("P", None, "peta", 1e15),
-    ("T", None, "tera", 1e12),
-    ("G", None, "giga", 1e9),
-    ("M", None, "mega", 1e6),
-    ("k", None, "kilo", 1e3) ]
+Magnitude = namedtuple("Magnitude", ["symbol", "special_symbol", "latex_symbol", "name", "scale"])
 
-smaller_magnitudes = [
-    ("m", None, "milli", 1e-3),
-    ("u", "µ", "micro", 1e-6),
-    ("n", None, "nano", 1e-9),
-    ("p", None, "pico", 1e-12),
-    ("f", None, "femto", 1e-15),
-    ("a", None, "atto", 1e-18)]
+bigger_magnitudes: list[Magnitude] = [
+    Magnitude("E", None, None, "exa", 1e18),
+    Magnitude("P", None, None, "peta", 1e15),
+    Magnitude("T", None, None, "tera", 1e12),
+    Magnitude("G", None, None, "giga", 1e9),
+    Magnitude("M", None, None, "mega", 1e6),
+    Magnitude("k", None, None, "kilo", 1e3) ]
 
-unusual_magnitudes = [
-    ("d", None, "deci", 1e-1),
-    ("c", None, "centi", 1e-2)
+smaller_magnitudes: list[Magnitude] = [
+    Magnitude("m", None, None, "milli", 1e-3),
+    Magnitude("u", "µ", r"\mu", "micro", 1e-6),
+    Magnitude("n", None, None, "nano", 1e-9),
+    Magnitude("p", None, None, "pico", 1e-12),
+    Magnitude("f", None, None, "femto", 1e-15),
+    Magnitude("a", None, None, "atto", 1e-18)]
+
+unusual_magnitudes: list[Magnitude] = [
+    Magnitude("d", None, None, "deci", 1e-1),
+    Magnitude("c", None, None, "centi", 1e-2)
 ]
 
 all_magnitudes = bigger_magnitudes + smaller_magnitudes
 
+UnitData = namedtuple("UnitData", ["symbol", "special_symbol", "latex_symbol", "singular", "plural", "scale", "length", "time", "mass", "current", "temperature", "moles_hint", "angle_hint", "magnitudes"])
+
 # Length, time, mass, current, temperature
 base_si_units = [
-    ("m", None, "meter", "meters", 1, 1, 0, 0, 0, 0, 0, 0, all_magnitudes + unusual_magnitudes),
-    ("s", None, "second", "seconds", 1, 0, 1, 0, 0, 0, 0, 0, smaller_magnitudes),
-    ("g", None, "gram", "grams", 1e-3, 0, 0, 1, 0, 0, 0, 0, all_magnitudes),
-    ("A", None, "ampere", "amperes", 1, 0, 0, 0, 1, 0, 0, 0, all_magnitudes),
-    ("K", None, "kelvin", "kelvin", 1, 0, 0, 0, 0, 1, 0, 0, all_magnitudes) ]
+    UnitData("m", None, None, "meter", "meters", 1, 1, 0, 0, 0, 0, 0, 0, all_magnitudes + unusual_magnitudes),
+    UnitData("s", None, None, "second", "seconds", 1, 0, 1, 0, 0, 0, 0, 0, smaller_magnitudes),
+    UnitData("g", None, None, "gram", "grams", 1e-3, 0, 0, 1, 0, 0, 0, 0, all_magnitudes),
+    UnitData("A", None, None, "ampere", "amperes", 1, 0, 0, 0, 1, 0, 0, 0, all_magnitudes),
+    UnitData("K", None, None, "kelvin", "kelvin", 1, 0, 0, 0, 0, 1, 0, 0, all_magnitudes) ]
 
 derived_si_units = [
-    ("Hz", None, "hertz", "hertz", 1, 0, -1, 0, 0, 0, 0, 0, all_magnitudes),
-    ("N", None, "newton", "newtons", 1, 1, -2, 1, 0, 0, 0, 0, all_magnitudes),
-    ("Pa", None, "pascal", "pascals", 1, -1, -2, 1, 0, 0, 0, 0, all_magnitudes),
-    ("J", None, "joule", "joules", 1, 2, -2, 1, 0, 0, 0, 0, all_magnitudes),
-    ("W", None, "watt", "watts", 1, 2, -3, 1, 0, 0, 0, 0, all_magnitudes),
-    ("C", None, "coulomb", "coulombs", 1, 0, 1, 0, 1, 0, 0, 0, all_magnitudes),
-    ("V", None, "volts", "volts", 1, 2, -3, 1, -1, 0, 0, 0, all_magnitudes),
-    ("Ohm", "Ω", "ohm", "ohms", 1, 2, -3, 1, -2, 0, 0, 0, all_magnitudes),
-    ("F", None, "farad", "farads", 1, -2, 4, -1, 2, 0, 0, 0, all_magnitudes),
-    ("S", None, "siemens", "siemens", 1, -2, 3, -1, 2, 0, 0, 0, all_magnitudes),
-    ("Wb", None, "weber", "webers", 1, 2, -2, 1, -1, 0, 0, 0, all_magnitudes),
-    ("T", None, "tesla", "tesla", 1, 0, -2, 1, -1, 0, 0, 0, all_magnitudes),
-    ("H", None, "henry", "henry", 1, 2, -2, 1, -2, 0, 0, 0, all_magnitudes),
+    UnitData("Hz", None, None, "hertz", "hertz", 1, 0, -1, 0, 0, 0, 0, 0, all_magnitudes),
+    UnitData("N", None, None, "newton", "newtons", 1, 1, -2, 1, 0, 0, 0, 0, all_magnitudes),
+    UnitData("Pa", None, None, "pascal", "pascals", 1, -1, -2, 1, 0, 0, 0, 0, all_magnitudes),
+    UnitData("J", None, None, "joule", "joules", 1, 2, -2, 1, 0, 0, 0, 0, all_magnitudes),
+    UnitData("W", None, None, "watt", "watts", 1, 2, -3, 1, 0, 0, 0, 0, all_magnitudes),
+    UnitData("C", None, None, "coulomb", "coulombs", 1, 0, 1, 0, 1, 0, 0, 0, all_magnitudes),
+    UnitData("V", None, None, "volts", "volts", 1, 2, -3, 1, -1, 0, 0, 0, all_magnitudes),
+    UnitData("Ohm", "Ω", r"\Omega", "ohm", "ohms", 1, 2, -3, 1, -2, 0, 0, 0, all_magnitudes),
+    UnitData("F", None, None, "farad", "farads", 1, -2, 4, -1, 2, 0, 0, 0, all_magnitudes),
+    UnitData("S", None, None, "siemens", "siemens", 1, -2, 3, -1, 2, 0, 0, 0, all_magnitudes),
+    UnitData("Wb", None, None, "weber", "webers", 1, 2, -2, 1, -1, 0, 0, 0, all_magnitudes),
+    UnitData("T", None, None, "tesla", "tesla", 1, 0, -2, 1, -1, 0, 0, 0, all_magnitudes),
+    UnitData("H", None, None, "henry", "henry", 1, 2, -2, 1, -2, 0, 0, 0, all_magnitudes),
 ]
 
 non_si_dimensioned_units: list[tuple[str, str | None, str, str, float, int, int, int, int, int, int, int, list]] = [
-    ("Ang", "Å", "angstrom", "angstroms", 1e-10, 1, 0, 0, 0, 0, 0, 0, []),
-    ("min", None, "minute", "minutes", 60, 0, 1, 0, 0, 0, 0, 0, []),
-    ("h", None, "hour", "hours", 360, 0, 1, 0, 0, 0, 0, 0, []),
-    ("d", None, "day", "days", 360*24, 0, 1, 0, 0, 0, 0, 0, []),
-    ("y", None, "year", "years", 360*24*365.2425, 0, 1, 0, 0, 0, 0, 0, []),
-    ("deg", None, "degree", "degrees", 180/np.pi, 0, 0, 0, 0, 0, 0, 1, []),
-    ("rad", None, "radian", "radians", 1, 0, 0, 0, 0, 0, 0, 1, []),
-    ("sr", None, "stradian", "stradians", 1, 0, 0, 0, 0, 0, 0, 2, []),
-    ("l", None, "litre", "litres", 1e-3, 3, 0, 0, 0, 0, 0, 0, []),
-    ("eV", None, "electronvolt", "electronvolts", 1.602176634e-19, 2, -2, 1, 0, 0, 0, 0, all_magnitudes),
-    ("au", None, "atomic mass unit", "atomic mass units", 1.660538921e-27, 0, 0, 1, 0, 0, 0, 0, []),
-    ("mol", None, "mole", "moles", 6.02214076e23, 0, 0, 0, 0, 0, 1, 0, smaller_magnitudes),
-    ("kgForce", None, "kg force", "kg force",  9.80665, 1, -2, 1, 0, 0, 0, 0, []),
-    ("C", None, "degree Celsius", "degrees Celsius", 1, 0, 0, 0, 0, 1, 0, 0, []),
-    ("miles", None, "mile", "miles", 1760*3*0.3048, 1, 0, 0, 0, 0, 0, 0, []),
-    ("yrd", None, "yard", "yards", 3*0.3048, 1, 0, 0, 0, 0, 0, 0, []),
-    ("ft", None, "foot", "feet", 0.3048, 1, 0, 0, 0, 0, 0, 0, []),
-    ("in", None, "inch", "inches", 0.0254, 1, 0, 0, 0, 0, 0, 0, []),
-    ("lb", None, "pound", "pounds", 0.45359237, 0, 0, 1, 0, 0, 0, 0, []),
-    ("lbf", None, "pound force", "pounds force", 4.448222, 1, -2, 1, 0, 0, 0, 0, []),
-    ("oz", None, "ounce", "ounces", 0.45359237/16, 0, 0, 1, 0, 0, 0, 0, []),
-    ("psi", None, "pound force per square inch", "pounds force per square inch", 4.448222/(0.0254**2), -1, -2, 1, 0, 0, 0, 0, []),
+    UnitData("Ang", "Å", r"\AA", "angstrom", "angstroms", 1e-10, 1, 0, 0, 0, 0, 0, 0, []),
+    UnitData("min", None, None, "minute", "minutes", 60, 0, 1, 0, 0, 0, 0, 0, []),
+    UnitData("h", None, None, "hour", "hours", 360, 0, 1, 0, 0, 0, 0, 0, []),
+    UnitData("d", None, None, "day", "days", 360*24, 0, 1, 0, 0, 0, 0, 0, []),
+    UnitData("y", None, None, "year", "years", 360*24*365.2425, 0, 1, 0, 0, 0, 0, 0, []),
+    UnitData("deg", None, None, "degree", "degrees", 180/np.pi, 0, 0, 0, 0, 0, 0, 1, []),
+    UnitData("rad", None, None, "radian", "radians", 1, 0, 0, 0, 0, 0, 0, 1, []),
+    UnitData("sr", None, None, "stradian", "stradians", 1, 0, 0, 0, 0, 0, 0, 2, []),
+    UnitData("l", None, None, "litre", "litres", 1e-3, 3, 0, 0, 0, 0, 0, 0, []),
+    UnitData("eV", None, None, "electronvolt", "electronvolts", 1.602176634e-19, 2, -2, 1, 0, 0, 0, 0, all_magnitudes),
+    UnitData("au", None, None, "atomic mass unit", "atomic mass units", 1.660538921e-27, 0, 0, 1, 0, 0, 0, 0, []),
+    UnitData("mol", None, None, "mole", "moles", 6.02214076e23, 0, 0, 0, 0, 0, 1, 0, smaller_magnitudes),
+    UnitData("kgForce", None, None, "kg force", "kg force",  9.80665, 1, -2, 1, 0, 0, 0, 0, []),
+    UnitData("C", None, None, "degree Celsius", "degrees Celsius", 1, 0, 0, 0, 0, 1, 0, 0, []),
+    UnitData("miles", None, None, "mile", "miles", 1760*3*0.3048, 1, 0, 0, 0, 0, 0, 0, []),
+    UnitData("yrd", None, None, "yard", "yards", 3*0.3048, 1, 0, 0, 0, 0, 0, 0, []),
+    UnitData("ft", None, None, "foot", "feet", 0.3048, 1, 0, 0, 0, 0, 0, 0, []),
+    UnitData("in", None, None, "inch", "inches", 0.0254, 1, 0, 0, 0, 0, 0, 0, []),
+    UnitData("lb", None, None, "pound", "pounds", 0.45359237, 0, 0, 1, 0, 0, 0, 0, []),
+    UnitData("lbf", None, None, "pound force", "pounds force", 4.448222, 1, -2, 1, 0, 0, 0, 0, []),
+    UnitData("oz", None, None, "ounce", "ounces", 0.45359237/16, 0, 0, 1, 0, 0, 0, 0, []),
+    UnitData("psi", None, None, "pound force per square inch", "pounds force per square inch", 4.448222/(0.0254**2), -1, -2, 1, 0, 0, 0, 0, []),
 ]
 
 non_si_dimensionless_units: list[tuple[str, str | None, str, str, float, int, int, int, int, int, int, int, list]] = [
-    ("none", None, "none", "none", 1, 0, 0, 0, 0, 0, 0, 0, []),
-    ("percent", "%", "percent", "percent", 0.01, 0, 0, 0, 0, 0, 0, 0, [])
+    UnitData("none", None, None, "none", "none", 1, 0, 0, 0, 0, 0, 0, 0, []),
+    UnitData("percent", "%", r"\%", "percent", "percent", 0.01, 0, 0, 0, 0, 0, 0, 0, [])
 ]
 
 non_si_units = non_si_dimensioned_units + non_si_dimensionless_units
@@ -152,51 +156,54 @@ with open("units.py", 'w', encoding=encoding) as fid:
 
     for unit_def in all_units:
 
-        try:
-            symbol, special_symbol, singular, plural, scale, length, time, \
-                mass, current, temperature, moles_hint, angle_hint, magnitudes = unit_def
-        except Exception as e:
-            print(unit_def)
-            raise e
+        formatted_plural = format_name(unit_def.plural)
+        formatted_singular = format_name(unit_def.singular)
 
-        formatted_plural = format_name(plural)
-        formatted_singular = format_name(singular)
-
-        dimensions = Dimensions(length, time, mass, current, temperature, moles_hint, angle_hint)
-        fid.write(f"{formatted_plural} = NamedUnit({scale}, Dimensions({length}, {time}, {mass}, {current}, {temperature}, {moles_hint}, {angle_hint}),"
+        dimensions = Dimensions(unit_def.length, unit_def.time, unit_def.mass, unit_def.current, unit_def.temperature, unit_def.moles_hint, unit_def.angle_hint)
+        fid.write(f"{formatted_plural} = NamedUnit({unit_def.scale}, Dimensions({unit_def.length}, {unit_def.time}, {unit_def.mass}, {unit_def.current}, {unit_def.temperature}, {unit_def.moles_hint}, {unit_def.angle_hint}),"
                       f"name='{formatted_plural}',"
-                      f"ascii_symbol='{symbol}',"
-                      f"symbol='{symbol if special_symbol is None else special_symbol}')\n")
+                      f"ascii_symbol='{unit_def.symbol}',"
+                      f"{'' if unit_def.latex_symbol is None else f"""latex_symbol=r'{unit_def.latex_symbol}',""" }"
+                      f"symbol='{unit_def.symbol if unit_def.special_symbol is None else unit_def.special_symbol}')\n")
 
-        symbol_lookup[symbol] = formatted_plural
-        if special_symbol is not None:
-            symbol_lookup[special_symbol] = formatted_plural
+        symbol_lookup[unit_def.symbol] = formatted_plural
+        if unit_def.special_symbol is not None:
+            symbol_lookup[unit_def.special_symbol] = formatted_plural
 
         unit_types_temp[hash(dimensions)].append(
-            (symbol, special_symbol, formatted_singular, formatted_plural, scale, dimensions))
+            (unit_def.symbol, unit_def.special_symbol, formatted_singular, formatted_plural, unit_def.scale, dimensions))
 
         unit_types[hash(dimensions)].append(formatted_plural)
 
-        for mag_symbol, mag_special_symbol, name, mag_scale in magnitudes:
+        for mag in unit_def.magnitudes:
 
             # Work out the combined symbol, accounts for unicode or not
-            combined_special_symbol = (mag_symbol if mag_special_symbol is None else mag_special_symbol) + \
-                              (symbol if special_symbol is None else special_symbol)
+            combined_special_symbol = (mag.symbol if mag.special_symbol is None else mag.special_symbol) + \
+                              (unit_def.symbol if unit_def.special_symbol is None else unit_def.special_symbol)
 
-            combined_symbol = mag_symbol + symbol
+            combined_symbol = mag.symbol + unit_def.symbol
 
             # Combined unit name
-            combined_name_singular = f"{name}{formatted_singular}"
-            combined_name_plural = f"{name}{formatted_plural}"
+            combined_name_singular = f"{mag.name}{formatted_singular}"
+            combined_name_plural = f"{mag.name}{formatted_plural}"
 
-            combined_scale = scale * mag_scale
+            combined_scale = unit_def.scale * mag.scale
+
+            latex_symbol = None
+            if unit_def.latex_symbol is not None and mag.latex_symbol is not None:
+                latex_symbol = f"{{{mag.latex_symbol}}}{unit_def.latex_symbol}"
+            elif unit_def.latex_symbol is not None:
+                latex_symbol = f"{mag.symbol}{unit_def.latex_symbol}"
+            elif mag.latex_symbol is not None:
+                latex_symbol = f"{{{mag.latex_symbol}}}{unit_def.symbol}"
 
             # Units
-            dimensions = Dimensions(length, time, mass, current, temperature, moles_hint, angle_hint)
+            dimensions = Dimensions(unit_def.length, unit_def.time, unit_def.mass, unit_def.current, unit_def.temperature, unit_def.moles_hint, unit_def.angle_hint)
             fid.write(f"{combined_name_plural} = NamedUnit({combined_scale}, "
-                      f"Dimensions({length}, {time}, {mass}, {current}, {temperature}, {moles_hint}, {angle_hint}),"
+                      f"Dimensions({unit_def.length}, {unit_def.time}, {unit_def.mass}, {unit_def.current}, {unit_def.temperature}, {unit_def.moles_hint}, {unit_def.angle_hint}),"
                       f"name='{combined_name_plural}',"
                       f"ascii_symbol='{combined_symbol}',"
+                      f"{'' if latex_symbol is None else f"""latex_symbol=r'{latex_symbol}',""" }"
                       f"symbol='{combined_special_symbol}')\n")
 
             symbol_lookup[combined_symbol] = combined_name_plural
@@ -426,7 +433,7 @@ with open("accessors.py", 'w', encoding=encoding) as fid:
 with open("si.py", 'w') as fid:
 
     fid.write('"""'+(warning_text%"_build_tables.py")+'"""\n\n')
-    si_unit_names = [values[3] for values in base_si_units + derived_si_units if values[3] != "grams"] + ["kilograms"]
+    si_unit_names = [values.plural for values in base_si_units + derived_si_units if values.plural != "grams"] + ["kilograms"]
 
     for name in si_unit_names:
 
