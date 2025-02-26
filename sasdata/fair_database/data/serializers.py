9e50e50f44ce40134bc1f383df3ec81158438b9b
summerhenson <summerh@gmail.com>
2025-02-26 16:14:50 -0500

    Bare bones serializers for database models

--- sasdata/fair_database/data/serializers.py
+++ sasdata/fair_database/data/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from data.models import DataFile
+from data.models import DataFile, DataSet, MetaData, OperationTree, Quantity
 
 
 class DataFileSerializer(serializers.ModelSerializer):
@@ -12,3 +12,27 @@ class DataFileSerializer(serializers.ModelSerializer):
         if not self.context["is_public"] and not data["current_user"]:
             raise serializers.ValidationError("private data must have an owner")
         return data
+
+
+class DataSetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataSet
+        fields = "__all__"
+
+
+class QuantitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quantity
+        fields = "__all__"
+
+
+class MetaDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MetaData
+        fields = "__all__"
+
+
+class OperationTreeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OperationTree
+        fields = "__all__"
