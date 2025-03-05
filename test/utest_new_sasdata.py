7a3b76706e2bf4efcbb7a74e09dd8c0b217a2206
James Crake-Merani <james@jamescm.co.uk>
2025-03-05 10:23:19 +0000

    Use brackets properly.

--- test/utest_new_sasdata.py
+++ test/utest_new_sasdata.py
@@ -45,4 +45,4 @@ def test_2d():
     data = SasData('TestData', data_contents, two_dim, Group('root', {}), True)
 
     assert all(data.ordinate.value == np.array(i))
-    assert data.abscissae.value == np.array([[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]).all().all()
+    assert (data.abscissae.value == np.array([[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]])).all().all()
