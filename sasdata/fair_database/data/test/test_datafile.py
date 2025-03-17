10874b479de2bb34db9fff385e695f0e6c5071fb
summerhenson <summerh@gmail.com>
2025-03-17 17:54:09 -0400

    Update tests for url changes

--- sasdata/fair_database/data/test/test_datafile.py
+++ sasdata/fair_database/data/test/test_datafile.py
@@ -42,38 +42,45 @@ class TestLists(TestCase):
 
     # Test list public data
     def test_does_list_public(self):
-        request = self.client1.get("/v1/data/list/")
-        self.assertEqual(request.data, {"public_data_ids": {1: "cyl_400_40.txt"}})
+        request = self.client1.get("/v1/data/file/")
+        self.assertEqual(
+            request.data,
+            {"public_data_ids": {1: "cyl_400_40.txt", 3: "cyl_400_20.txt"}},
+        )
 
     # Test list a user's private data
     def test_does_list_user(self):
-        request = self.client1.get("/v1/data/list/testUser/", user=self.user)
+        request = self.client1.get(
+            "/v1/data/file/", data={"username": "testUser"}, user=self.user
+        )
         self.assertEqual(request.data, {"user_data_ids": {3: "cyl_400_20.txt"}})
 
     # Test list another user's public data
     def test_list_other_user(self):
         client2 = APIClient()
-        request = client2.get("/v1/data/list/testUser/", user=self.user)
+        request = client2.get(
+            "/v1/data/file/", data={"username": "testUser"}, user=self.user
+        )
         self.assertEqual(request.data, {"user_data_ids": {}})
 
     # Test list a nonexistent user's data
     def test_list_nonexistent_user(self):
-        request = self.client1.get("/v1/data/list/fakeUser/")
+        request = self.client1.get("/v1/data/file/", data={"username": "fakeUser"})
         self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
 
     # Test loading a public data file
     def test_does_load_data_info_public(self):
-        request = self.client1.get("/v1/data/load/1/")
+        request = self.client1.get("/v1/data/file/1/")
         self.assertEqual(request.status_code, status.HTTP_200_OK)
 
     # Test loading private data with authorization
     def test_does_load_data_info_private(self):
-        request = self.client1.get("/v1/data/load/3/")
+        request = self.client1.get("/v1/data/file/3/")
         self.assertEqual(request.status_code, status.HTTP_200_OK)
 
     # Test loading data that does not exist
     def test_load_data_info_nonexistent(self):
-        request = self.client1.get("/v1/data/load/5/")
+        request = self.client1.get("/v1/data/file/5/")
         self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
 
     @classmethod
@@ -104,7 +111,7 @@ class TestingDatabase(APITestCase):
     def test_is_data_being_created(self):
         file = open(find("cyl_400_40.txt"), "rb")
         data = {"is_public": False, "file": file}
-        request = self.client1.post("/v1/data/upload/", data=data)
+        request = self.client1.post("/v1/data/file/", data=data)
         max_id = DataFile.objects.aggregate(Max("id"))["id__max"]
         self.assertEqual(request.status_code, status.HTTP_201_CREATED)
         self.assertEqual(
@@ -123,7 +130,7 @@ class TestingDatabase(APITestCase):
     def test_is_data_being_created_no_user(self):
         file = open(find("cyl_testdata.txt"), "rb")
         data = {"is_public": True, "file": file}
-        request = self.client2.post("/v1/data/upload/", data=data)
+        request = self.client2.post("/v1/data/file/", data=data)
         max_id = DataFile.objects.aggregate(Max("id"))["id__max"]
         self.assertEqual(request.status_code, status.HTTP_201_CREATED)
         self.assertEqual(
@@ -142,7 +149,7 @@ class TestingDatabase(APITestCase):
     def test_no_data_overwrite(self):
         file = open(find("apoferritin.txt"))
         data = {"is_public": True, "file": file, id: 1}
-        request = self.client1.post("/v1/data/upload/", data=data)
+        request = self.client1.post("/v1/data/file/", data=data)
         self.assertEqual(request.status_code, status.HTTP_201_CREATED)
         self.assertEqual(DataFile.objects.get(id=1).file_name, "cyl_400_20.txt")
         max_id = DataFile.objects.aggregate(Max("id"))["id__max"]
@@ -162,7 +169,7 @@ class TestingDatabase(APITestCase):
     def test_does_file_upload_update(self):
         file = open(find("cyl_testdata1.txt"))
         data = {"file": file, "is_public": False}
-        request = self.client1.put("/v1/data/upload/1/", data=data)
+        request = self.client1.put("/v1/data/file/1/", data=data)
         self.assertEqual(
             request.data,
             {
@@ -186,7 +193,7 @@ class TestingDatabase(APITestCase):
         )
         file = open(find("conalbumin.txt"))
         data = {"file": file, "is_public": True}
-        request = self.client1.put("/v1/data/upload/3/", data=data)
+        request = self.client1.put("/v1/data/file/3/", data=data)
         self.assertEqual(
             request.data,
             {
@@ -203,19 +210,19 @@ class TestingDatabase(APITestCase):
     def test_unauthorized_file_upload_update(self):
         file = open(find("cyl_400_40.txt"))
         data = {"file": file, "is_public": False}
-        request = self.client2.put("/v1/data/upload/1/", data=data)
+        request = self.client2.put("/v1/data/file/1/", data=data)
         self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # Test update nonexistent file fails
     def test_file_upload_update_not_found(self):
         file = open(find("cyl_400_40.txt"))
         data = {"file": file, "is_public": False}
-        request = self.client2.put("/v1/data/upload/5/", data=data)
+        request = self.client2.put("/v1/data/file/5/", data=data)
         self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
 
     # Test file download
     def test_does_download(self):
-        request = self.client1.get("/v1/data/1/download/")
+        request = self.client1.get("/v1/data/file/1/", data={"download": True})
         file_contents = b"".join(request.streaming_content)
         test_file = open(find("cyl_400_20.txt"), "rb")
         self.assertEqual(request.status_code, status.HTTP_200_OK)
@@ -223,12 +230,12 @@ class TestingDatabase(APITestCase):
 
     # Test file download fails when unauthorized
     def test_unauthorized_download(self):
-        request2 = self.client2.get("/v1/data/1/download/")
+        request2 = self.client2.get("/v1/data/file/1/", data={"download": True})
         self.assertEqual(request2.status_code, status.HTTP_401_UNAUTHORIZED)
 
     # Test download nonexistent file
     def test_download_nonexistent(self):
-        request = self.client1.get("/v1/data/5/download/")
+        request = self.client1.get("/v1/data/file/5/", data={"download": True})
         self.assertEqual(request.status_code, status.HTTP_404_NOT_FOUND)
 
     # Test deleting a file
@@ -236,13 +243,13 @@ class TestingDatabase(APITestCase):
         DataFile.objects.create(
             id=6, current_user=self.user, file_name="test.txt", is_public=False
         )
-        request = self.client1.delete("/v1/data/delete/6/")
+        request = self.client1.delete("/v1/data/file/6/")
         self.assertEqual(request.status_code, status.HTTP_200_OK)
         self.assertFalse(DataFile.objects.filter(pk=6).exists())
 
     # Test deleting a file fails when unauthorized
     def test_delete_unauthorized(self):
-        request = self.client2.delete("/v1/data/delete/1/")
+        request = self.client2.delete("/v1/data/file/1/")
         self.assertEqual(request.status_code, status.HTTP_401_UNAUTHORIZED)
 
     @classmethod
@@ -279,14 +286,14 @@ class TestAccessManagement(TestCase):
 
     # test viewing no one with access
     def test_view_no_access(self):
-        request = self.client1.get("/v1/data/manage/1/")
+        request = self.client1.get("/v1/data/file/1/users/")
         data = {"file": 1, "file_name": "cyl_400_40.txt", "users": []}
         self.assertEqual(request.status_code, status.HTTP_200_OK)
         self.assertEqual(request.data, data)
 
     # test viewing list of users with access
     def test_view_access(self):
-        request = self.client1.get("/v1/data/manage/2/")
+        request = self.client1.get("/v1/data/file/2/users/")
         data = {"file": 2, "file_name": "cyl_400_20.txt", "users": ["testUser2"]}
         self.assertEqual(request.status_code, status.HTTP_200_OK)
         self.assertEqual(request.data, data)
@@ -294,17 +301,17 @@ class TestAccessManagement(TestCase):
     # test granting another user access to private data
     def test_grant_access(self):
         data = {"username": "testUser2", "access": True}
-        request1 = self.client1.put("/v1/data/manage/1/", data=data)
-        request2 = self.client2.get("/v1/data/load/1/")
+        request1 = self.client1.put("/v1/data/file/1/users/", data=data)
+        request2 = self.client2.get("/v1/data/file/1/")
         self.assertEqual(request1.status_code, status.HTTP_200_OK)
         self.assertEqual(request2.status_code, status.HTTP_200_OK)
 
     # test removing another user's access to private data
     def test_remove_access(self):
         data = {"username": "testUser2", "access": False}
-        request1 = self.client2.get("/v1/data/load/2/")
-        request2 = self.client1.put("/v1/data/manage/2/", data=data)
-        request3 = self.client2.get("/v1/data/load/2/")
+        request1 = self.client2.get("/v1/data/file/2/")
+        request2 = self.client1.put("/v1/data/file/2/users/", data=data)
+        request3 = self.client2.get("/v1/data/file/2/")
         self.assertEqual(request1.status_code, status.HTTP_200_OK)
         self.assertEqual(request2.status_code, status.HTTP_200_OK)
         self.assertEqual(request3.status_code, status.HTTP_403_FORBIDDEN)
@@ -312,9 +319,9 @@ class TestAccessManagement(TestCase):
     # test removing access from a user that already lacks access
     def test_remove_no_access(self):
         data = {"username": "testUser2", "access": False}
-        request1 = self.client2.get("/v1/data/load/1/")
-        request2 = self.client1.put("/v1/data/manage/1/", data=data)
-        request3 = self.client2.get("/v1/data/load/1/")
+        request1 = self.client2.get("/v1/data/file/1/")
+        request2 = self.client1.put("/v1/data/file/1/users/", data=data)
+        request3 = self.client2.get("/v1/data/file/1/")
         self.assertEqual(request1.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(request2.status_code, status.HTTP_200_OK)
         self.assertEqual(request3.status_code, status.HTTP_403_FORBIDDEN)
@@ -322,17 +329,17 @@ class TestAccessManagement(TestCase):
     # test owner's access cannot be removed
     def test_cant_revoke_own_access(self):
         data = {"username": "testUser", "access": False}
-        request1 = self.client1.put("/v1/data/manage/1/", data=data)
-        request2 = self.client1.get("/v1/data/load/1/")
+        request1 = self.client1.put("/v1/data/file/1/users/", data=data)
+        request2 = self.client1.get("/v1/data/file/1/")
         self.assertEqual(request1.status_code, status.HTTP_200_OK)
         self.assertEqual(request2.status_code, status.HTTP_200_OK)
 
     # test giving access to a user that already has access
     def test_grant_existing_access(self):
         data = {"username": "testUser2", "access": True}
-        request1 = self.client2.get("/v1/data/load/2/")
-        request2 = self.client1.put("/v1/data/manage/2/", data=data)
-        request3 = self.client2.get("/v1/data/load/2/")
+        request1 = self.client2.get("/v1/data/file/2/")
+        request2 = self.client1.put("/v1/data/file/2/users/", data=data)
+        request3 = self.client2.get("/v1/data/file/2/")
         self.assertEqual(request1.status_code, status.HTTP_200_OK)
         self.assertEqual(request2.status_code, status.HTTP_200_OK)
         self.assertEqual(request3.status_code, status.HTTP_200_OK)
@@ -340,14 +347,14 @@ class TestAccessManagement(TestCase):
     # test that access is read-only for the file
     def test_no_edit_access(self):
         data = {"is_public": True}
-        request = self.client2.put("/v1/data/upload/2/", data=data)
+        request = self.client2.put("/v1/data/file/2/", data=data)
         self.assertEqual(request.status_code, status.HTTP_403_FORBIDDEN)
         self.assertFalse(self.shared_test_data.is_public)
 
     # test that only the owner can view who has access
     def test_only_view_access_to_owned_file(self):
-        request1 = self.client2.get("/v1/data/manage/1/")
-        request2 = self.client2.get("/v1/data/manage/2/")
+        request1 = self.client2.get("/v1/data/file/1/users/")
+        request2 = self.client2.get("/v1/data/file/2/users/")
         self.assertEqual(request1.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(request2.status_code, status.HTTP_403_FORBIDDEN)
 
@@ -355,10 +362,10 @@ class TestAccessManagement(TestCase):
     def test_only_edit_access_to_owned_file(self):
         data1 = {"username": "testUser2", "access": True}
         data2 = {"username": "testUser1", "access": False}
-        request1 = self.client2.put("/v1/data/manage/1/", data=data1)
-        request2 = self.client2.put("/v1/data/manage/2/", data=data2)
-        request3 = self.client2.get("/v1/data/load/1/")
-        request4 = self.client1.get("/v1/data/load/2/")
+        request1 = self.client2.put("/v1/data/file/1/users/", data=data1)
+        request2 = self.client2.put("/v1/data/file/2/users/", data=data2)
+        request3 = self.client2.get("/v1/data/file/1/")
+        request4 = self.client1.get("/v1/data/file/2/")
         self.assertEqual(request1.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(request2.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(request3.status_code, status.HTTP_403_FORBIDDEN)
