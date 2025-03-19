b96c62cb4f5200f5e41bc2716334a1b726823fad
summerhenson <summerh@gmail.com>
2025-03-19 16:05:18 -0400

    Update metadata serializers for latest changes

--- sasdata/metadata.py
+++ sasdata/metadata.py
@@ -20,12 +20,40 @@ class Vec3:
     y : Quantity[float] | None
     z : Quantity[float] | None
 
+    def serialise_json(self):
+        data = {
+            "x": None,
+            "y": None,
+            "z": None
+        }
+        if self.x is not None:
+            data["x"] = self.x.serialise_json()
+        if self.y is not None:
+            data["y"] = self.y.serialise_json()
+        if self.z is not None:
+            data["z"] = self.z.serialise_json()
+        return data
+
 @dataclass(kw_only=True)
 class Rot3:
     """A measured rotation in 3-space"""
     roll : Quantity[float] | None
     pitch : Quantity[float] | None
     yaw : Quantity[float] | None
+
+    def serialise_json(self):
+        data = {
+            "roll": None,
+            "pitch": None,
+            "yaw": None
+        }
+        if self.roll is not None:
+            data["roll"] = self.roll.serialise_json()
+        if self.pitch is not None:
+            data["pitch"] = self.pitch.serialise_json()
+        if self.yaw is not None:
+            data["yaw"] = self.yaw.serialise_json()
+        return data
 
 @dataclass(kw_only=True)
 class Detector:
@@ -52,15 +80,28 @@ class Detector:
                 f"   Slit length:  {self.slit_length}\n")
 
     def serialise_json(self):
-        return {
-            "name": self.name.value,
-            "distance": self.distance.value.serialise_json(),
-            "offset": self.offset.value.serialise_json(),
-            "orientation": self.orientation.value.serialise_json(),
-            "beam_center": self.beam_center.value.serialise_json(),
-            "pixel_size": self.pixel_size.value.serialise_json(),
-            "slit_length": self.slit_length.value.serialise_json()
+        data = {
+            "name": self.name,
+            "distance": None,
+            "offset": None,
+            "orientation": None,
+            "beam_center": None,
+            "pixel_size": None,
+            "slit_length": None
         }
+        if self.distance is not None:
+            data["distance"] = self.distance.serialise_json()
+        if self.offset is not None:
+            data["offset"] = self.offset.serialise_json()
+        if self.orientation is not None:
+            data["orientation"] = self.orientation.serialise_json()
+        if self.beam_center is not None:
+            data["beam_center"] = self.beam_center.serialise_json()
+        if self.pixel_size is not None:
+            data["pixel_size"] = self.pixel_size.serialise_json()
+        if self.slit_length is not None:
+            data["slit_length"] = self.slit_length.serialise_json()
+        return data
 
 
 @dataclass(kw_only=True)
@@ -76,6 +117,19 @@ class Aperture:
                 f"  Name: {self.name}\n"
                 f"  Aperture size: {self.size}\n"
                 f"  Aperture distance: {self.distance}\n")
+
+    def serialise_json(self):
+        data = {
+            "distance": None,
+            "size": None,
+            "size_name": self.size_name,
+            "name": self.name,
+            "type": self.type_
+        }
+        if self.distance is not None:
+            data["distance"] = self.distance.serialise_json()
+        if self.size is not None:
+            data["size"] = self.size.serialise_json()
 
 @dataclass(kw_only=True)
 class Collimation:
@@ -94,15 +148,27 @@ class Collimation:
             f"   Length: {self.length}\n")
 
     def serialise_json(self):
-        return {
-            "name": self.name.value,
-            "length": self.length.value.serialise_json()
+        data = {
+            "length": None,
+            "apertures": [a.serialise_json() for a in self.apertures]
         }
+        if self.length is not None:
+            data["length"] = self.length.serialise_json()
+        return data
 
 @dataclass(kw_only=True)
 class BeamSize:
     name: str | None
     size: Vec3 | None
+
+    def serialise_json(self):
+        data = {
+            "name": self.name,
+            "size": None
+        }
+        if self.size is not None:
+            data["size"] = self.size.serialise_json()
+        return data
 
 @dataclass(kw_only=True)
 class Source:
@@ -125,6 +191,28 @@ class Source:
             f"    Wavelength Spread: {self.wavelength_spread}\n"
             f"    Beam Size:         {self.beam_size}\n"
         )
+
+    def serialise_json(self):
+        data = {
+            "radiation": self.radiation,
+            "beam_shape": self.beam_shape,
+            "beam_size": None,
+            "wavelength": None,
+            "wavelength_min": None,
+            "wavelength_max": None,
+            "wavelength_spread": None
+        }
+        if self.beam_size is not None:
+            data["beam_size"] = self.beam_size.serialise_json()
+        if self.wavelength is not None:
+            data["wavelength"] = self.wavelength.serialise_json()
+        if self.wavelength_min is not None:
+            data["wavelength_min"] = self.wavelength_min.serialise_json()
+        if self.wavelength_max is not None:
+            data["wavelength_max"] = self.wavelength_max.serialise_json()
+        if self.wavelength_spread is not None:
+            data["wavelength_spread"] = self.wavelength_spread.serialise_json()
+        return data
 
 @dataclass(kw_only=True)
 class Sample:
@@ -150,16 +238,25 @@ class Sample:
                 f"   Orientation:  {self.orientation}\n")
 
     def serialise_json(self):
-        return {
-            "name": self.name.value,
-            "sample_id": self.sample_id.value,
-            "thickness": self.thickness.value.serialise_json(),
-            "transmission": self.transmission.value,
-            "temperature": self.temperature.value.serialise_json(),
-            "position": self.position.value.serialise_json(),
-            "orientation": self.orientation.value.serialise_json(),
-            "details": self.details.value
+        data = {
+            "name": self.name,
+            "sample_id": self.sample_id,
+            "thickness": None,
+            "transmission": self.transmission,
+            "temperature": None,
+            "position": None,
+            "orientation": None,
+            "details": self.details
         }
+        if self.thickness is not None:
+            data["thickness"] = self.thickness.serialise_json()
+        if self.temperature is not None:
+            data["temperature"] = self.temperature.serialise_json()
+        if self.position is not None:
+            data["position"] = self.position.serialise_json()
+        if self.orientation is not None:
+            data["orientation"] = self.orientation.serialise_json()
+        return data
 
 
 @dataclass(kw_only=True)
@@ -177,7 +274,7 @@ class Process:
         """
             Return a single line string representing the process
         """
-        return f"{self.name.value} {self.date.value} {self.description.value}"
+        return f"{self.name} {self.date} {self.description}"
 
     def summary(self):
         return (f"Process:\n"
@@ -189,11 +286,10 @@ class Process:
 
     def serialise_json(self):
         return {
-            "name": "",
-            "date": "",
-            "description": "",
-            "term": "",
-            "notes": ""
+            "name": self.name,
+            "date": self.date,
+            "description": self.description,
+            "term": self.term,
         }
 
 
@@ -208,6 +304,16 @@ class Instrument:
             "\n".join([c.summary() for c in self.collimations]) +
             "".join([d.summary() for d in self.detector]) +
             self.source.summary())
+
+    def serialise_json(self):
+        data = {
+            "collimations": [c.serialise_json() for c in self.collimations],
+            "source": None,
+            "detector": [d.serialise_json() for d in self.detector]
+        }
+        if self.source is not None:
+            data["source"] = self.source.serialise_json()
+        return data
 
 @dataclass(kw_only=True)
 class Metadata:
@@ -231,12 +337,17 @@ class Metadata:
             (self.instrument.summary() if self.instrument else ""))
 
     def serialise_json(self):
-        return {
-            "instrument": self.instrument.serialise_json(),
-            "process": self.process.serialise_json(),
-            "sample": self.sample.serialise_json(),
-            "transmission_spectrum": self.transmission_spectrum.serialise_json(),
+        serialized = {
+            "instrument": None,
+            "process": [p.serialise_json() for p in self.process],
+            "sample": None,
             "title": self.title,
             "run": self.run,
             "definition": self.definition
         }
+        if self.sample is not None:
+            serialized["sample"] = self.sample.serialise_json()
+        if self.instrument is not None:
+            serialized["instrument"] = self.instrument.serialise_json()
+
+        return serialized
