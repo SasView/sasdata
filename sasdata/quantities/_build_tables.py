c8725d7b0c2ff43a7cd46b84c7a4d22aad0a682e
Adam Washington <adam.washington@stfc.ac.uk>
2025-04-08 14:36:45 +0100

    Add micron unit alias

--- sasdata/quantities/_build_tables.py
+++ sasdata/quantities/_build_tables.py
@@ -60,6 +60,7 @@ derived_si_units = [
 
 non_si_dimensioned_units: list[tuple[str, str | None, str, str, float, int, int, int, int, int, int, int, list]] = [
     UnitData("Ang", "Å", r"\AA", "angstrom", "angstroms", 1e-10, 1, 0, 0, 0, 0, 0, 0, []),
+    UnitData("micron", None, None, "micron", "microns", 1e-6, 1, 0, 0, 0, 0, 0, 0, []),
     UnitData("min", None, None, "minute", "minutes", 60, 0, 1, 0, 0, 0, 0, 0, []),
     UnitData("h", None, None, "hour", "hours", 360, 0, 1, 0, 0, 0, 0, 0, []),
     UnitData("d", None, None, "day", "days", 360*24, 0, 1, 0, 0, 0, 0, 0, []),
