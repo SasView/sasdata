f90bafa3c1b492d89e5e5a7e7d4701cc882641e0
summerhenson <summerh@gmail.com>
2025-04-14 16:04:31 -0400

    Test listing sessions

--- sasdata/fair_database/data/test/test_session.py
+++ sasdata/fair_database/data/test/test_session.py
@@ -1,18 +1,99 @@
-from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+
+
+from data.models import Session
 
 
 class TestSession(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        pass
+        cls.user1 = User.objects.create_user(
+            id=1, username="testUser1", password="secret"
+        )
+        cls.user2 = User.objects.create_user(
+            id=2, username="testUser2", password="secret"
+        )
+        cls.public_session = Session.objects.create(
+            id=1, current_user=cls.user1, title="Public Session", is_public=True
+        )
+        cls.private_session = Session.objects.create(
+            id=2, current_user=cls.user1, title="Private Session", is_public=False
+        )
+        cls.unowned_session = Session.objects.create(
+            id=3, title="Unowned Session", is_public=True
+        )
+        cls.auth_client1 = APIClient()
+        cls.auth_client2 = APIClient()
+        cls.auth_client1.force_authenticate(cls.user1)
+        cls.auth_client2.force_authenticate(cls.user2)
 
     # Test listing sessions
-    # Can see own private
-    # Can't see other private
-    # Can see private with access
-    # Can see public (authenticated and unauthenticated)
+    def test_list_private(self):
+        request = self.auth_client1.get("/v1/data/session/")
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            request.data,
+            {
+                "session_ids": {
+                    1: "Public Session",
+                    2: "Private Session",
+                    3: "Unowned Session",
+                }
+            },
+        )
 
-    # Test listing sessions by username
+    # Test listing public sessions
+    def test_list_public(self):
+        request = self.auth_client2.get("/v1/data/session/")
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            request.data, {"session_ids": {1: "Public Session", 3: "Unowned Session"}}
+        )
+
+    # Test listing sessions while unauthenticated
+    def test_list_unauthenticated(self):
+        request = self.client.get("/v1/data/session/")
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            request.data, {"session_ids": {1: "Public Session", 3: "Unowned Session"}}
+        )
+
+    # Test listing a session with access granted
+    def test_list_granted_access(self):
+        self.private_session.users.add(self.user2)
+        request = self.auth_client2.get("/v1/data/session/")
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            request.data,
+            {
+                "session_ids": {
+                    1: "Public Session",
+                    2: "Private Session",
+                    3: "Unowned Session",
+                }
+            },
+        )
+        self.private_session.users.remove(self.user2)
+
+    # Test listing by username
+    def test_list_username(self):
+        request = self.auth_client1.get(
+            "/v1/data/session/", data={"username": "testUser1"}
+        )
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            request.data, {"session_ids": {1: "Public Session", 2: "Private Session"}}
+        )
+
+    # Test listing by another user's username
+    def test_list_other_username(self):
+        request = self.auth_client2.get(
+            "/v1/data/session/", data={"username": "testUser1"}
+        )
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(request.data, {"session_ids": {1: "Public Session"}})
 
     # Test creating a session - public, private, unauthenticated
     # Datasets have same access as session
@@ -21,7 +102,11 @@ class TestSession(APITestCase):
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        cls.public_session.delete()
+        cls.private_session.delete()
+        cls.unowned_session.delete()
+        cls.user1.delete()
+        cls.user2.delete()
 
 
 class TestSingleSession(APITestCase):
