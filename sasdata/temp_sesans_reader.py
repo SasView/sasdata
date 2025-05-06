619ad5b4d11f318b880f9b88269470d2f536f3e6
Adam Washington <adam.washington@stfc.ac.uk>
2025-05-06 14:06:15 +0100

    Skeleton framework for SESANS data

--- sasdata/temp_sesans_reader.py
+++ sasdata/temp_sesans_reader.py
@@ -2,68 +2,29 @@
 Import SESANS data in SasData format
 """
 
-import re
-from collections import defaultdict
-from itertools import groupby
-
-import numpy as np
-
 from sasdata.data import SasData
 from sasdata.data_util.loader_exceptions import FileContentsException
-from sasdata.dataset_types import sesans
-from sasdata.metadata import (
-    Metadata,
-    MetaNode,
-    Process,
-    Sample,
-)
-from sasdata.quantities import unit_parser, units
+from sasdata.dataset_types import one_dim
 from sasdata.quantities.quantity import Quantity
+from sasdata.metadata import Metadata, Sample
 
 
 def parse_version(lines: list[str]) -> tuple[str, list[str]]:
+    import re
+
     header = lines[0]
-    m = re.search(r"FileFormatVersion\s+(\S+)", header)
+    m = re.search("FileFormatVersion\s+(\S+)", header)
 
     if m is None:
-        raise FileContentsException(
-            "Sesans file does not contain File Format Version header"
-        )
+        raise FileContentsException("Alleged Sesans file does not contain File Format Version header")
 
     return (m.group(0), lines[1:])
 
-
-def parse_title(kvs: dict[str, str]) -> str:
-    """Get the title from the key value store"""
-    if "Title" in kvs:
-        return kvs["Title"]
-    elif "DataFileTitle" in kvs:
-        return kvs["DataFileTitle"]
-    for k, v in kvs.items():
-        if "Title" in k:
-            return v
-    return ""
-
-
-def parse_kvs_quantity(key: str, kvs: dict[str, str]) -> Quantity | None:
-    if key not in kvs or key + "_unit" not in kvs:
-        return None
-    return Quantity(value=float(kvs[key]), units=unit_parser.parse(kvs[key + "_unit"]))
-
-
-def parse_sample(kvs: dict[str, str]) -> Sample:
-    """Get the sample info from the key value store"""
-
-    thickness = parse_kvs_quantity("Thickness", kvs)
-    if thickness is None:
-        raise FileContentsException(
-            "SES format must include sample thickness to normalise calculations"
-        )
-
-    return Sample(
-        name=kvs.get("Sample"),
+def parse_metadata(lines: list[str]) -> tuple[Metadata, list[str]]:
+    sample = Sample(
+        name=None,
         sample_id=None,
-        thickness=thickness,
+        thickness=None,
         transmission=None,
         temperature=None,
         position=None,
@@ -71,133 +32,31 @@ def parse_sample(kvs: dict[str, str]) -> Sample:
         details=[],
     )
 
-
-def parse_process(kvs: dict[str, str]) -> Process:
-    ymax = parse_kvs_quantity("Theta_ymax", kvs)
-    zmax = parse_kvs_quantity("Theta_zmax", kvs)
-    orientation = kvs.get("Orientation")
-
-    if ymax is None:
-        raise FileContentsException("SES file must specify Theta_ymax")
-    if zmax is None:
-        raise FileContentsException("SES file must specify Theta_zmax")
-    if orientation is None:
-        raise FileContentsException("SES file must include encoding orientation")
-
-    terms: dict[str, str | Quantity] = {
-        "ymax": ymax,
-        "zmax": zmax,
-        "orientation": orientation,
-    }
-
-    return Process(
-        name="SESANS Processing",
-        date=None,
-        description="Polarisation measurement through a SESANS instrument",
-        terms=terms,
-        notes=[],
-    )
-
-
-def parse_metanode(kvs: dict[str, str]) -> MetaNode:
-    """Convert header into metanode"""
-    contents: list[MetaNode] = []
-    title = parse_title(kvs)
-
-    for k, v in kvs.items():
-        if v.endswith("_unit") and v[:-5] in kvs:
-            # This is the unit for another term
-            continue
-        if v + "_unit" in kvs:
-            contents.append(
-                MetaNode(
-                    name=k,
-                    attrs={},
-                    contents=Quantity(
-                        value=float(v), units=unit_parser.parse(kvs[k + "_unit"])
-                    ),
-                )
-            )
-        else:
-            contents.append(MetaNode(name=k, attrs={}, contents=v))
-
-    return MetaNode(name=title, attrs={}, contents=contents)
-
-
-def parse_metadata(lines: list[str]) -> tuple[Metadata, dict[str, str], list[str]]:
-    parts = [
-        [y for y in x]
-        for (_, x) in groupby(lines, lambda x: x.startswith("BEGIN_DATA"))
-    ]
-
-    if len(parts) != 3:
-        raise FileContentsException("SES file should have exactly one data section")
-
-    # Parse key value store
-    kvs: dict[str, str] = {}
-    for line in parts[0]:
-        m = re.search(r"(\S+)\s+(.+)\n", line)
-        if not m:
-            continue
-        kvs[m.group(1)] = m.group(2)
-
     return (
         Metadata(
-            process=[parse_process(kvs)],
+            process=[],
             instrument=None,
-            sample=parse_sample(kvs),
-            title=parse_title(kvs),
+            sample=sample,
+            title="Title",
             run=[],
             definition=None,
-            raw=parse_metanode(kvs),
         ),
-        kvs,
-        parts[2],
+        lines,
     )
 
 
-def parse_data(lines: list[str], kvs: dict[str, str]) -> dict[str, Quantity]:
-
+def parse_data(lines: list[str]) -> dict[str, Quantity]:
     data_contents: dict[str, Quantity] = {}
-    headers = lines[0].split()
-    points = defaultdict(list)
-    for line in lines[1:]:
-        values = line.split()
-        for idx, v in enumerate(values):
-            points[headers[idx]].append(float(v))
-
-    for h in points:
-        if h.endswith("_error") and h[:-6] in headers:
-            # This was an error line
-            continue
-        unit = units.none
-        if h + "_unit" in kvs:
-            unit = unit_parser.parse(kvs[h + "_unit"])
-
-        error = None
-        if h + "_error" in headers:
-            error = np.asarray(points[h + "_error"])
-
-        data_contents[h] = Quantity(
-            value=np.asarray(points[h]),
-            units=unit,
-            standard_error=error,
-        )
-
-    for required in ["SpinEchoLength", "Depolarisation", "Wavelength"]:
-        if required not in data_contents:
-            raise FileContentsException(f"SES file missing {required}")
-
     return data_contents
 
 
 def parse_sesans(lines: list[str]) -> SasData:
     version, lines = parse_version(lines)
-    metadata, kvs, lines = parse_metadata(lines)
-    data_contents = parse_data(lines, kvs)
+    metadata, lines = parse_metadata(lines)
+    data_contents = parse_data(lines)
     return SasData(
         name="Sesans",
-        dataset_type=sesans,
+        dataset_type=one_dim,
         data_contents=data_contents,
         metadata=metadata,
         verbose=False,
--- test/sasdataloader/utest_new_sesans.py
+++ test/sasdataloader/utest_new_sesans.py
@@ -3,24 +3,11 @@ Unit tests for the new recursive cansas reader
 """
 
 import os
-
-import numpy as np
 import pytest
 
-from sasdata.model_requirements import (
-    ComposeRequirements,
-    ModellingRequirements,
-    NullModel,
-    PinholeModel,
-    SesansModel,
-    SlitModel,
-    guess_requirements,
-)
-from sasdata.quantities import unit_parser, units
-from sasdata.quantities.quantity import Quantity
-from sasdata.temp_sesans_reader import load_data
 
-test_file_names = ["sphere2micron", "sphere_isis"]
+from sasdata.temp_hdf5_reader import load_data
+from sasdata.temp_sesans_reader import load_data
 
 
 def local_load(path: str):
@@ -29,107 +16,9 @@ def local_load(path: str):
 
 
 @pytest.mark.sesans
-@pytest.mark.parametrize("f", test_file_names)
-def test_load_file(f):
-    data = load_data(local_load(f"sesans_data/{f}.ses"))
+def test_load_file():
+    data = load_data(local_load(f"sesans_data/sphere2micron.ses"))
 
-    with open(local_load(f"reference/{f}.txt")) as infile:
+    with open(local_load(f"reference/sphere2micron.txt")) as infile:
         expected = "".join(infile.readlines())
     assert data.summary() == expected
-
-@pytest.mark.sesans
-def test_sesans_modelling():
-    data = load_data(local_load("sesans_data/sphere2micron.ses"))
-    req = guess_requirements(data)
-    assert type(req) is SesansModel
-
-    def form_volume(x):
-        return np.pi * 4.0 / 3.0 * x**3
-
-    radius = 10000
-    contrast = 5.4e-7 # Contrast is hard coded for best fit
-    form = contrast * form_volume(radius)
-    f2 = 1.0e-4*form*form
-
-    xi_squared = smear(req, data._data_contents["SpinEchoLength"].value, full_data=data, y = data._data_contents["Depolarisation"].in_units_of(unit_parser.parse("A-2 cm-1")) / f2, radius=radius)
-    assert 1.0 < xi_squared < 1.5
-
-@pytest.mark.sesans
-def test_pinhole_zero():
-    assert pinhole_smear(0) == 0
-
-@pytest.mark.sesans
-def test_pinhole_smear():
-    smearing = [3**x for x in range(-1, 6)]
-    smears = [pinhole_smear(x) for x in smearing]
-    old = 0
-    for factor, smear in zip(smearing, smears):
-        print(factor, smear)
-        assert old < smear < 1.2
-        old = smear
-    assert pinhole_smear(3**6) > 1.2
-
-@pytest.mark.sesans
-def test_slit_zero():
-    assert slit_smear(0, 0) == 0
-
-@pytest.mark.sesans
-def test_slit_smear():
-    smears = [(length, width, slit_smear(3**length, 3**width)) for length in range(-1, 6) for width in range(-1, 6) if length + width == 6]
-    for length, width, smear in smears:
-        print(length, width, smear)
-        assert smear < 1.2
-    assert slit_smear(3**6, 1) > 1.2
-    assert slit_smear(1, 3**6) > 1.2
-
-
-def slit_smear(length: float, width):
-    data = Quantity(np.linspace(1e-4, 1e-1, 1000), units.per_angstrom)
-    diff = np.diff(data.value, prepend=0)
-    req = SlitModel(diff * length, diff * width)
-    return smear(req, data.value)
-
-
-def pinhole_smear(smearing: float):
-    data = Quantity(np.linspace(1e-4, 1e-1, 1000), units.per_angstrom)
-    req = PinholeModel(np.diff(data.value, prepend=0) * smearing)
-    return smear(req, data.value)
-
-
-def smear(req: ModellingRequirements, data: np.ndarray, y=None, full_data=None, radius=200):
-    def sphere(q):
-        def sas_3j1x_x(x):
-            return (np.sin(x) - x * np.cos(x))/x**3
-
-        return sas_3j1x_x(q * radius)**2
-
-    inner_q = req.preprocess_q(data, full_data)
-    result = req.postprocess_iq(sphere(inner_q), data)
-
-    if y is None:
-        y = sphere(data)
-
-    xi_squared = np.sum( ((y - result)/result )**2 ) / len(y)
-    return xi_squared
-
-
-
-@pytest.mark.sesans
-def test_model_algebra():
-    ses = SesansModel()
-    pin = PinholeModel(np.linspace(1e-3, 1, 1000))
-    slit = SlitModel(np.linspace(1e-3, 1, 1000), np.linspace(1e-3, 1, 1000))
-    null = NullModel()
-
-    # Ignore slit smearing if we perform a sesans transform afterwards
-    assert type(pin + ses) is SesansModel
-    assert type(slit + ses) is SesansModel
-    # However, it is possible for the spin echo lengths to have some
-    # smearing between them.
-    assert type(ses + pin) is ComposeRequirements
-    assert type(null + ses) is SesansModel
-    assert type(null + slit) is SlitModel
-    assert type(null + pin) is PinholeModel
-    assert type(ses + null) is SesansModel
-    assert type(pin + null) is PinholeModel
-    assert type(slit + null) is SlitModel
