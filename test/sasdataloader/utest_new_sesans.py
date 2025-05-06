dab5956f3c811c6f2460d2fac9ff4ac70c201cad
Adam Washington <adam.washington@stfc.ac.uk>
2025-05-06 16:49:16 +0100

    Multiple SESANS files in reader test

--- test/sasdataloader/utest_new_sesans.py
+++ test/sasdataloader/utest_new_sesans.py
@@ -9,6 +9,8 @@ import pytest
 from sasdata.temp_hdf5_reader import load_data
 from sasdata.temp_sesans_reader import load_data
 
+test_file_names = ["sphere2micron", "sphere_isis"]
+
 
 def local_load(path: str):
     """Get local file path"""
@@ -16,9 +18,10 @@ def local_load(path: str):
 
 
 @pytest.mark.sesans
-def test_load_file():
-    data = load_data(local_load(f"sesans_data/sphere2micron.ses"))
+@pytest.mark.parametrize("f", test_file_names)
+def test_load_file(f):
+    data = load_data(local_load(f"sesans_data/{f}.ses"))
 
-    with open(local_load(f"reference/sphere2micron.txt")) as infile:
+    with open(local_load(f"reference/{f}.txt")) as infile:
         expected = "".join(infile.readlines())
     assert data.summary() == expected
