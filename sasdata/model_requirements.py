40a4d54f14bcb8307491cb3c161d52979db73ee7
Adam Washington <adam.washington@stfc.ac.uk>
2025-06-18 14:09:36 +0100

    Start implementing ModellingRequirements

--- sasdata/model_requirements.py
+++ sasdata/model_requirements.py
@@ -1,23 +1,78 @@
-from dataclasses import dataclass
+from abs import ABC
 
 import numpy as np
 
-from sasdata.metadata import Metadata
+from sasdata.data import SasData
+from sasdata import dataset_types
 from transforms.operation import Operation
 
 
-@dataclass
-class ModellingRequirements:
-    """ Requirements that need to be passed to any modelling step """
+class ModellingRequirements(ABC):
+    """Requirements that need to be passed to any modelling step"""
+
     dimensionality: int
     operation: Operation
 
-    def from_qi_transformation(self, data: np.ndarray, metadata: Metadata) -> np.ndarray:
-        """ Transformation for going from qi to this data"""
+    def __add__(self, other: ModellingRequirements):
+        return compose(self, other)
+
+    @abstractmethod
+    def from_qi_transformation(
+        self, data: np.ndarray, metadata: Metadata
+    ) -> np.ndarray:
+        """Transformation for going from qi to this data"""
         pass
 
 
+class ComposeRequirements(ModellingRequirements):
+    """Composition of two models"""
+
+    first: ModellingRequirements
+    second: ModellingRequirements
+
+    def from_qi_transformation(
+        self, data: np.ndarray, metadata: Metadata
+    ) -> np.ndarray:
+        """Perform both transformations in order"""
+        return self.second.from_qi_transformation(
+            self.first.from_qi_transformation(data, metadata), metadata
+        )
 
 
-def guess_requirements(abscissae, ordinate) -> ModellingRequirements:
-    """ Use names of axes and units to guess what kind of processing needs to be done """
+class SesansModel(ModellingRequirements):
+    """Perform Hankel transform for SESANS"""
+
+    def from_qi_transformation(
+        self, data: np.ndarray, metadata: Metadata
+    ) -> np.ndarray:
+        """Perform Hankel transform"""
+        # FIXME: Actually do the Hankel transform
+       return data
+
+class SmearModel(ModellingRequirements):
+    """Perform a slit smearing"""
+
+    def from_qi_transformation(
+        self, data: np.ndarray, metadata: Metadata
+    ) -> np.ndarray:
+        """Perform smearing transfor"""
+        # FIXME: Actually do the smearing transform
+       return data
+
+class NullModel(ModellingRequirements):
+    """A model that does nothing"""
+
+    def from_qi_transformation(self, data: np.ndarray, _metadata: Metadata) -> np.ndarray:
+        """Do nothing"""
+        return data
+
+def compose(a: ModellingRequirements, b: ModellingRequirements) -> ModellingRequirements:
+    return ComposeRequirements(a, b)
+
+
+
+def guess_requirements(data: SasData) -> ModellingRequirements:
+    """Use names of axes and units to guess what kind of processing needs to be done"""
+    if data.dataset_type == dataset_types.sesans:
+        return SesansModel() + SmearModel()
+    pass
