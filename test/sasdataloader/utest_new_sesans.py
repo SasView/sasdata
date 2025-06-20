e4338f5827168fcbccc7d6bef811d935cc6ee3a2
Adam Washington <adam.washington@stfc.ac.uk>
2025-06-20 15:34:37 +0100

    χ² squared based test

--- test/sasdataloader/utest_new_sesans.py
+++ test/sasdataloader/utest_new_sesans.py
@@ -7,8 +7,9 @@ import os
 import pytest
 
 
-from sasdata.temp_sesans_reader import load_data
 from sasdata.model_requirements import guess_requirements, ComposeRequirements, SmearModel, SesansModel, NullModel
+from sasdata.temp_sesans_reader import load_data
+from sasdata.quantities import units, unit_parser
 
 test_file_names = ["sphere2micron", "sphere_isis"]
 
@@ -30,25 +31,34 @@ def test_load_file(f):
 @pytest.mark.sesans
 def test_sesans_modelling():
     import matplotlib.pyplot as plt
-    data = load_data(local_load("sesans_data/sphere_isis.ses"))
+    data = load_data(local_load("sesans_data/sphere2micron.ses"))
     req = guess_requirements(data)
     assert type(req) is SesansModel
 
-    # The Hankel transform of x is -r^-3
-    x = np.arange(0.0, len(data._data_contents["Wavelength"].value))
-    inner_q = req.preprocess_q(x, data)
-    # Just use y=1/x as our model
-    iq = inner_q[:]**-1.0
-    result = req.postprocess_iq(iq, data)
 
-    # plt.loglog()
-    # plt.plot(x, 1/(2 * np.pi) * x**-1, label="Expected")
-    # plt.plot(x, result, label="Computed")
-    # plt.legend(loc=0)
-    # plt.show()
-    assert x.shape == result.shape
-    for (expected, computed) in zip(-x**-3, result):
-        assert expected == computed
+    def sphere(qr):
+        def sas_3j1x_x(x):
+            return (np.sin(x) - x * np.cos(x))/x**3
+        def form_volume(x):
+            return np.pi * 4.0 / 3.0 * x**3
+        radius = 10000 # 1 micron
+
+        bes = sas_3j1x_x(q*radius)
+        contrast = 5.4e-7 # Contrast is hard coded for best fit
+        form = contrast * form_volume(radius) * bes
+        f2 = 1.0e-4*form*form
+        return f2
+
+    # The Hankel transform of x is -r^-3
+    x = data._data_contents["SpinEchoLength"].in_units_of(units.angstroms)
+    q = req.preprocess_q(x, data)
+    result = req.postprocess_iq(sphere(q), data)
+
+    y, yerr = data._data_contents["Depolarisation"].in_units_of_with_standard_error(unit_parser.parse("A-2 cm-1"))
+    assert y.shape == result.shape
+
+    xi_squared = np.sum( ((y - result) / yerr)**2 ) / len(y)
+    assert 1.0 < xi_squared < 1.5
 
 
 
