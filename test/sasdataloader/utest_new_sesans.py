24c67c3907c914685f4d80b221c1b118af837e2a
Adam Washington <adam.washington@stfc.ac.uk>
2025-07-14 11:20:38 +0100

    Basic Pinhole model

--- test/sasdataloader/utest_new_sesans.py
+++ test/sasdataloader/utest_new_sesans.py
@@ -7,8 +7,9 @@ import os
 import pytest
 
 
-from sasdata.model_requirements import guess_requirements, ComposeRequirements, SmearModel, SesansModel, NullModel
+from sasdata.model_requirements import guess_requirements, ComposeRequirements, PinholeModel, SesansModel, NullModel
 from sasdata.temp_sesans_reader import load_data
+from sasdata.quantities.quantity import Quantity
 from sasdata.quantities import units, unit_parser
 
 test_file_names = ["sphere2micron", "sphere_isis"]
@@ -58,20 +59,91 @@ def test_sesans_modelling():
     xi_squared = np.sum( ((y - result) / yerr)**2 ) / len(y)
     assert 1.0 < xi_squared < 1.5
 
+@pytest.mark.sesans
+def test_pinhole_zero():
+    data = Quantity(np.linspace(1e-4, 1e-1, 1000), units.per_angstrom)
+    req = PinholeModel()
+
+
+    def sphere(qr):
+        def sas_3j1x_x(x):
+            return (np.sin(x) - x * np.cos(x))/x**3
+        def form_volume(x):
+            return np.pi * 4.0 / 3.0 * x**3
+        radius = 10000 # 1 micron
+
+        bes = sas_3j1x_x(q*radius)
+        contrast = 5.4e-7 # Contrast is hard coded for best fit
+        form = contrast * form_volume(radius) * bes
+        f2 = 1.0e-4*form*form
+        return f2
+
+    # The Hankel transform of x is -r^-3
+    q = req.preprocess_q(data, None)
+    result = req.postprocess_iq(sphere(q), data)
+
+    for actual, expected in zip(result, sphere(q)):
+        assert actual == expected
+
+@pytest.mark.sesans
+def test_pinhole_smear():
+    smearing = [1000, 100, 20, 15, 10, 2, 1, 0.5, 0.2]
+    smears = [pinhole_smear(x) for x in smearing]
+    old = 0
+    for factor, smear in zip(smearing, smears):
+        print(factor, smear)
+        assert old < smear < 1.2
+        old = smear
+    assert pinhole_smear(3**6) > 1.2
+
+
+def pinhole_smear(smearing: float):
+    data = Quantity(np.linspace(1e-4, 1e-1, 1000), units.per_angstrom)
+    data = data.with_standard_error(data / smearing)
+    req = PinholeModel()
+
+
+    def sphere(qr):
+        def sas_3j1x_x(x):
+            return (np.sin(x) - x * np.cos(x))/x**3
+        def form_volume(x):
+            return np.pi * 4.0 / 3.0 * x**3
+        radius = 10000 # 1 micron
+
+        bes = sas_3j1x_x(qr)
+        contrast = 5.4e-7 # Contrast is hard coded for best fit
+        form = contrast * form_volume(radius) * bes
+        f2 = 1.0e-4*form*form
+        return f2
+
+    radius = 200
+
+    # The Hankel transform of x is -r^-3
+    inner_q = req.preprocess_q(data, None)
+    result = req.postprocess_iq(sphere(inner_q * radius), data)
+
+    # for actual, expected in zip(result, sphere(data.value * radius)):
+    #     assert actual != expected
+
+    y = sphere(data.value * radius)
+
+    xi_squared = np.sum( ((y - result)/result )**2 ) / len(y)
+    return xi_squared
+
 
 
 @pytest.mark.sesans
 def test_model_algebra():
     ses = SesansModel()
-    sme = SmearModel()
+    pin = PinholeModel()
     null = NullModel()
 
     # Ignore slit smearing if we perform a sesans transform afterwards
-    assert type(sme + ses) is SesansModel
+    assert type(pin + ses) is SesansModel
     # However, it is possible for the spin echo lengths to have some
     # smearing between them.
-    assert type(ses + sme) is ComposeRequirements
+    assert type(ses + pin) is ComposeRequirements
     assert type(null + ses) is SesansModel
-    assert type(null + sme) is SmearModel
+    assert type(null + pin) is PinholeModel
     assert type(ses + null) is SesansModel
-    assert type(sme + null) is SmearModel
+    assert type(pin + null) is PinholeModel
