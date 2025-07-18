d036066fa6a8cc5365f26db34abceccd1e036888
James Crake-Merani <James.Crake-Merani@stfc.ac.uk>
2025-07-18 10:29:34 +0100

    Added a get default unit function.
    Also Ruff format.

--- sasdata/default_units.py
+++ sasdata/default_units.py
@@ -5,11 +5,12 @@ import sasdata.quantities.units as unit
 from sasdata.dataset_types import unit_kinds
 
 default_units = {
-    'Q': [unit.per_nanometer, unit.per_angstrom, unit.per_meter],
-    'I': [unit.per_centimeter, unit.per_meter],
-    'dQ': 'Q',
-    'dI': 'I'
+    "Q": [unit.per_nanometer, unit.per_angstrom, unit.per_meter],
+    "I": [unit.per_centimeter, unit.per_meter],
+    "dQ": "Q",
+    "dI": "I",
 }
+
 
 def defaults_or_fallback(column_name: str) -> list[NamedUnit]:
     value = default_units.get(column_name, unit_kinds[column_name].units)
@@ -17,5 +18,13 @@ def defaults_or_fallback(column_name: str) -> list[NamedUnit]:
         return defaults_or_fallback(value)
     return value
 
+
 def first_default_for_fallback(column_name: str) -> NamedUnit:
     return defaults_or_fallback(column_name)[0]
+
+
+def get_default_unit(column_name: str, unit_group: unit.UnitGroup):
+    value = first_default_for_fallback(column_name)
+    # Fallback to the first unit in the unit group if we don't have a default registered.
+    if value is None:
+        return unit_group.units[0]
