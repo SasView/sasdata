008317f44462e814840d21b6816c35f40b1a26d4
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-08-01 13:50:48 +0100

    Fixes ruff linting errors (#140)
    * Fixes ruff linting errors
    
    * Adds ruff to pyproject.toml
    
    * Further fixes for refactor branch
    
    * Addresses review comments

--- test/utest_unit_parser.py
+++ test/utest_unit_parser.py
@@ -42,6 +42,11 @@ def test_scale_same(string: str, expected_units: Unit):
     """ Test basic parsing"""
     assert parse_unit(string).scale == pytest.approx(expected_units.scale, rel=1e-14)
 
+@pytest.mark.parametrize("latex_string, units", latex_units_for_testing)
+def test_scale_same_latex(latex_string: str, units: Unit):
+    """ Test that proper LaTeX formats for units are being generated"""
+    assert units.latex_symbol == latex_string
+
 
 def test_parse_from_group():
     """ Test group based disambiguation"""
