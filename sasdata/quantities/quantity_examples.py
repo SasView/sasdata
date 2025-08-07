5d8b77c12b057ed799e5c91c10b79a1688639cc0
Paul Sharp <44529197+DrPaulSharp@users.noreply.github.com>
2025-08-07 09:23:27 +0100

    Applies fixes for isort (I) ruff ruleset (#146)

--- sasdata/quantities/quantity_examples.py
+++ sasdata/quantities/quantity_examples.py
@@ -1,5 +1,5 @@
-from sasdata.quantities.quantity import NamedQuantity
 from sasdata.quantities import units
+from sasdata.quantities.quantity import NamedQuantity
 
 x = NamedQuantity("x", 1, units.meters, standard_error=1)
 y = NamedQuantity("y", 1, units.decimeters, standard_error=1)
