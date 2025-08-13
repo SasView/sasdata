e10fef9961585e715b73d606228a5fefbdeaecf6
Adam Washington <adam.washington@stfc.ac.uk>
2025-08-13 10:27:44 +0100

    Enumerate keys for access

--- test/sasdataloader/utest_metadata_tags.py
+++ test/sasdataloader/utest_metadata_tags.py
@@ -4,7 +4,7 @@ Unit tests for metadata tag access
 
 import pytest
 
-from sasdata.metadata import Metadata, Process, access_meta
+from sasdata.metadata import Metadata, Process, access_meta, meta_tags
 
 
 @pytest.mark.sasdata3
@@ -24,3 +24,10 @@ def test_tag_access():
     assert access_meta(meta, ".run") == [5, 11]
     assert access_meta(meta, ".process")[0].name == "Frobulator"
     assert access_meta(meta, ".process[0].name") == "Frobulator"
+
+@pytest.mark.sasdata3
+def test_tag_listing():
+    processes = [Process(name="Frobulator", date=None, description=None, terms={}, notes=[])]
+    meta = Metadata(title="Example", run=[5, 11], definition="An example metadata set", process=processes, sample=None, instrument=None, raw=None)
+
+    assert sorted(meta_tags(meta)) == sorted([".raw", ".sample", ".instrument", ".definition", ".title", ".run[0]", ".run[1]", ".process[0].terms", ".process[0].date", ".process[0].name", ".process[0].description"])
