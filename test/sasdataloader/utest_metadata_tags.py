d309e10e125a68731e490bd2cf7709208b27935f
Adam Washington <adam.washington@stfc.ac.uk>
2025-08-13 12:22:56 +0100

    Enable tag collection

--- test/sasdataloader/utest_metadata_tags.py
+++ test/sasdataloader/utest_metadata_tags.py
@@ -4,7 +4,7 @@ Unit tests for metadata tag access
 
 import pytest
 
-from sasdata.metadata import Metadata, Process, access_meta, meta_tags
+from sasdata.metadata import Metadata, Process, access_meta, meta_tags, collect_tags
 
 
 @pytest.mark.sasdata3
@@ -55,3 +55,54 @@ def test_tag_listing():
         ".sample",
         ".title",
     ]
+
+
+@pytest.mark.sasdata3
+def test_tag_merge():
+    """Check ability to access terms by name"""
+    processes = [
+        Process(
+            name="Frobulator",
+            date=None,
+            description=None,
+            terms={"Bobbin": "Threadbare", "Guybrush": "Threepwood"},
+            notes=[],
+        )
+    ]
+    meta = Metadata(
+        title="Example",
+        run=[5, 11],
+        definition="An example metadata set",
+        process=processes,
+        sample=None,
+        instrument=None,
+        raw=None,
+    )
+
+    processes2 = [
+        Process(
+            name="Frobulator",
+            date=None,
+            description=None,
+            terms={"Bobbin": "Threadbare", "Doctor Ed": "Edison"},
+            notes=[],
+        )
+    ]
+    meta2 = Metadata(
+        title="Example 2",
+        run=[5, 11, 12],
+        definition="A second example metadata set",
+        process=processes2,
+        sample=None,
+        instrument=None,
+        raw=None,
+    )
+
+    result = collect_tags([meta, meta2])
+
+    assert result.singular == set([".process[0].name", '.process[0].date', '.process[0].description', '.process[0].terms["Bobbin"]', ".run[0]", ".run[1]",  ".sample", ".instrument", ".raw"])
+
+    # Only meta2 has a run[2] term, so it cannot be collected
+    assert ".run[2]" not in result.singular
+
+    assert result.variable == set([".definition", ".title"])
