48842bfae4b84668d330656011fbe4bfde205be8
Paul Neves <paulneves77@gmail.com>
2025-11-14 12:21:21 +0100

    added rebin and sketch of inplied qz for postprocessing

--- sasdata/postprocess.py
+++ sasdata/postprocess.py
@@ -4,6 +4,10 @@ Post processing for loaded files
 
 """
 
+import numpy as np
+
+from sasdata.data import SasData
+
 def fix_mantid_units_error(data: SasData) -> SasData:
     pass
 
@@ -12,5 +16,34 @@ def fix_mantid_units_error(data: SasData) -> SasData:
 def apply_fixes(data: SasData, mantid_unit_error=True):
     if mantid_unit_error:
         data = fix_mantid_units_error(data)
+
+    return data
+
+
+def deduce_qz(data: SasData):
+    # check if metadata has wavelength information and data is 2D
+    wavelength = getattr(
+        getattr(
+            getattr(data, "metadata", None),
+            "source",
+            None
+        ),
+        "wavelength",
+        None
+    )
+    if wavelength is not None:
+        # we can deduce the value of qz from qx and qy
+        qx = data._data_contents['Qx']
+        qy = data._data_contents['Qy']
+
+        # this is how you convert qx, qy, and wavelength to qz
+        k0 = 2*np.pi/wavelength
+        twotheta = np.arcsin((qx**2 + qy**2) / k0)
+        qz = (1 - np.cos(twotheta)) * k0
+
+        data._data_contents['Qz'] = qz
+    else:
+        # we are making the approximation that qz=0
+        data._data_contents['Qz'] = 0*data._data_contents['Qx']
 
     return data
