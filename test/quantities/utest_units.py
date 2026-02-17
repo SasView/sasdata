bb377a2be050c2c168f349fc07b27115c9d77350
Adam Washington <adam.washington@stfc.ac.uk>
2026-02-17 11:56:28 +0000

    Ensure that frequency and angular frequency are distinct

--- test/quantities/utest_units.py
+++ test/quantities/utest_units.py
@@ -26,6 +26,16 @@ class EquivalentButUnequalUnits:
                 assert unit_1.equivalent(unit_2), "Units should be equivalent"
                 assert unit_1 != unit_2, "Units should not be equal"
 
+class DissimilarUnits:
+    def __init__(self, test_name: str, *units):
+        self.test_name = "Dissimilar: " + test_name
+        self.units: list[Unit] = list(units)
+
+    def run_test(self):
+        for i, unit_1 in enumerate(self.units):
+            for unit_2 in self.units[i+1:]:
+                assert not unit_1.equivalent(unit_2), "Units should not be equivalent"
+
 
 tests = [
 
@@ -46,6 +56,10 @@ tests = [
     EqualUnits("Angular frequency",
                (units.rotations/units.minutes ),
                (units.radians*units.hertz) * 2 * math.pi/60.0),
+
+    DissimilarUnits("Frequency and Angular frequency",
+                    (units.rotations/units.minutes),
+                    (units.hertz)),
 
 
 ]
