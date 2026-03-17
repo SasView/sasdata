afc6caad1e60edbd07f9fae03c1211c6deffe30b
krzywon <jkrzywon@nist.gov>
2026-03-17 09:34:20 -0400

    Use quantities for AnnularSector.py slicer values to ensure units are consistent

--- sasdata/slicing/slicers/AnnularSector.py
+++ sasdata/slicing/slicers/AnnularSector.py
@@ -1,20 +1,42 @@
 import numpy as np
 
+from sasdata.quantities.quantity import Quantity
+from sasdata.quantities.units import per_angstrom, radians
 from sasdata.slicing.meshes.mesh import Mesh
 from sasdata.slicing.rebinning import Rebinner
+
+QuantityType = Quantity | float
 
 
 class AnnularSector(Rebinner):
     """ A single annular sector (wedge sum)"""
-    def __init__(self, q0: float, q1: float, phi0: float, phi1: float, points_per_degree: int=2):
+    def __init__(self, q0: QuantityType, q1: QuantityType, phi0: QuantityType,
+                 phi1: QuantityType, points_per_degree: int=2):
         super().__init__()
 
-        self.q0 = q0
-        self.q1 = q1
-        self.phi0 = phi0
-        self.phi1 = phi1
+        # Ensure all values are scaled to the proper units
+        self._q0 = q0.to_units_of(per_angstrom) if isinstance(q0, Quantity) else Quantity(q0, per_angstrom)
+        self._q1 = q1.to_units_of(per_angstrom) if isinstance(q1, Quantity) else Quantity(q1, per_angstrom)
+        self._phi0 = phi0.to_units_of(radians) if isinstance(phi0, Quantity) else Quantity(phi0, radians)
+        self._phi1 = phi1.to_units_of(radians) if isinstance(phi1, Quantity) else Quantity(phi1, radians)
 
         self.points_per_degree = points_per_degree
+
+    @property
+    def q0(self):
+        return self._q0.value
+
+    @property
+    def q1(self):
+        return self._q1.value
+
+    @property
+    def phi0(self):
+        return self._phi0.value
+
+    @property
+    def phi1(self):
+        return self._phi1.value
 
     def _bin_mesh(self) -> Mesh:
 
