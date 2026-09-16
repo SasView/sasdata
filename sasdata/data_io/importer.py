56586fab0a775afb674821be8c7b92f81c84ddca
krzywon <jkrzywon@nist.gov>
2026-09-16 17:01:27 -0400

    Create base import method in Importer class

--- sasdata/data_io/importer.py
+++ sasdata/data_io/importer.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from sasdata.data import SasData
+from sasdata.data import SasMeasurement
+from sasdata.data_io.importers import *
 
 Path_Type = str | Path
 
@@ -9,13 +10,21 @@ class Importer:
     def __init__(self):
         pass
 
-    def import_data(self, files: Path_Type) -> (list[SasData], list[str]):
+    def import_data(self, files: Path_Type) -> (list[SasMeasurement], list[str]):
         output = []
         errors = []
         for file in files:
             file = Path(file)
-            if file.exists():
-                pass
+            if measurements := self._import_from_url(file):
+                output.append(measurements)
             else:
                 errors.append(f'File "{file}" not found.')
         return output, errors
+
+    def _import_from_url(self, url: Path_Type) -> list[SasMeasurement]:
+        file = Path(url)
+        if not file.exists():
+            return []
+        with open(url) as f:
+            pass
+        return []
